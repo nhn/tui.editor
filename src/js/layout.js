97f67f7a47ade8b398a9952508e9e2c14262a2d4
Sungho Kim <sungho-kim@nhnent.com>
2015-03-05 21:58:16 +0900

    ff에서 contentEditable영역이 필요이상으로 큰상황에서 첫입력시 개행이후에 텍스트가 입력되는 문제가 있다.

--- src/js/layout.js
+++ src/js/layout.js
@@ -23,7 +23,8 @@ Layout.prototype.init = function() {
 };
 
 Layout.prototype._initEditorEl = function() {
-    this.$editorEl = $('<pre contenteditable="true" class="language-markdown" style="height:100px;width:500px;overflow:hidden" />');
+    //height를 여기서 조절하면 ff에서 첫입력시 개행의 LF가 앞에 오는 경우가 있다. editorel은 사이즈를 고정하지말고 이것을 감싸는것으로 고정하자
+    this.$editorEl = $('<div contenteditable="true" class="language-markdown" style="white-space: pre;" />');
     this.$el.append(this.$editorEl);
 };
 
