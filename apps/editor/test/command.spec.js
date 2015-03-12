9ca1535f5673b054e346a3a68a3bd706b021d99b
Sungho Kim <sungho-kim@nhnent.com>
2015-03-12 14:51:39 +0900

    불필요한 TC정리

---
+++ apps/editor/test/command.spec.js
@@ -0,0 +1,15 @@
+var Command = require('../src/js/command');
+
+describe('', function() {
+    'use strict';
+
+    beforeEach(function() {
+
+    });
+
+    describe('newLine()', function() {
+        it('에디팅영역이 비었다면', function() {
+            expect(true).toEqual(true);
+        });
+    });
+});
