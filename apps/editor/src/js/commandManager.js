287f87d05220373fbe35cfbc30d4c1414d7dccb0
Sungho Kim <sungho-kim@nhnent.com>
2015-03-16 18:47:40 +0900

    uic수정

---
+++ apps/editor/src/js/commandManager.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview
+ * @author FE개발팀 김성호 sungho-kim@nhnent.com
+ */
+
+'use strict';
+
+var util = ne.util;
+
+/**
+ * CommandManager
+ * @exports CommandManager
+ * @extends {}
+ * @constructor
+ * @class
+ */
+function CommandManager(base) {
+    this.commands = new util.HashMap();
+    this.base = base;
+}
+
+CommandManager.prototype.addCommand = function(commandOptions) {
+    this.commands.set(commandOptions.name, commandOptions.method);
+};
+
+CommandManager.prototype.action = function() {
+    var args = util.toArray(arguments),
+        name = args.shift(),
+        command = this.commands.get(name);
+
+    args.unshift(this.base);
+
+    if (command) {
+        command.apply(null, args);
+    }
+};
+
+module.exports = CommandManager;
