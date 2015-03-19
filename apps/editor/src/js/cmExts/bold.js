4c1cd37c91fa4146ee6486f030d18c508ee3c835
신승엽/협업시스템개발팀 <flysky@nhnent.com>
2015-03-19 14:23:03 +0900

    selection이 없을 때 bold 동작.

--- apps/editor/src/js/cmExts/bold.js
+++ apps/editor/src/js/cmExts/bold.js
@@ -11,31 +11,53 @@ var Bold = {
 
         cm.execCommand('singleSelection');
 
-        function isNeedRemove(text) {
-            return boldRegex.test(text);
+        function isNeedRemove() {
+            return boldRegex.test(selection);
         }
 
-        function append(text) {
-            return '**' + text + '**';
+        function append() {
+            return '**' + selection + '**';
         }
 
-        function remove(text) {
-            return text.substr(2, text.length - 4);
+        function remove() {
+            return selection.substr(2, selection.length - 4);
+        }
+
+        function expendSelection() {
+            doc.setSelection({line: cursor.line, ch: cursor.ch - 2}, {line: cursor.line, ch: cursor.ch + 2});
+
+            var tmpSelection = doc.getSelection();
+            if (tmpSelection === '****' || tmpSelection === '____') {
+                selection = tmpSelection;
+            } else {
+                doc.setSelection({line: cursor.line, ch: cursor.ch});
+            }
+        }
+
+        function setCursorToCenter() {
+            doc.setCursor(cursor.line, cursor.ch + 2);
         }
 
         var doc = cm.getDoc();
         var cursor = doc.getCursor();
         var selection = doc.getSelection();
-        var result = isNeedRemove(selection) ? remove(selection) : append(selection);
+        var isEmpty = !selection;
+
+        // if selection is empty, expend selection to detect a syntax
+        if (isEmpty && cursor.ch > 1) {
+            expendSelection();
+        }
+
+        var isRemoved = isNeedRemove();
+        var result = isRemoved ? remove() : append();
 
         console.log(selection);
         console.log(result);
 
         doc.replaceSelection(result, 'around');
 
-        if (!selection) {
-            console.log(cursor);
-            doc.setCursor(cursor.line, cursor.ch + 2);
+        if (isEmpty && !isRemoved) {
+            setCursorToCenter();
         }
     },
     keyMap: ['Ctrl-B', 'Cmd-B']
