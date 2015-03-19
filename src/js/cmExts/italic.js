23fb80611830b370aecc26ddd253578068dfd9f5
신승엽/협업시스템개발팀 <flysky@nhnent.com>
2015-03-19 15:06:23 +0900

    selection 없을 때 bold, italic 복합 동작.

--- src/js/cmExts/italic.js
+++ src/js/cmExts/italic.js
@@ -24,6 +24,28 @@ var Italic = {
             return selection.substr(1, selection.length - 2);
         }
 
+        function expendWithBoldSelection() {
+            doc.setSelection({line: cursor.line, ch: cursor.ch - 3}, {line: cursor.line, ch: cursor.ch + 3});
+
+            var tmpSelection = doc.getSelection();
+            if (tmpSelection === '******' || tmpSelection === '______') {
+                selection = tmpSelection;
+                isWithBold = 'with';
+            } else {
+                doc.setSelection(cursor);
+            }
+        }
+
+        function expendOnlyBoldSelection() {
+            doc.setSelection({line: cursor.line, ch: cursor.ch - 2}, {line: cursor.line, ch: cursor.ch + 2});
+
+            var tmpSelection = doc.getSelection();
+            if (tmpSelection === '****' || tmpSelection === '____') {
+                doc.setSelection(cursor);
+                isWithBold = 'only';
+            }
+        }
+
         function expendSelection() {
             doc.setSelection({line: cursor.line, ch: cursor.ch - 1}, {line: cursor.line, ch: cursor.ch + 1});
 
@@ -36,17 +58,29 @@ var Italic = {
         }
 
         function setCursorToCenter() {
-            doc.setCursor(cursor.line, cursor.ch + 1);
+            var pos = isRemoved ? -1 : 1;
+            doc.setCursor(cursor.line, cursor.ch + pos);
         }
 
         var doc = cm.getDoc();
         var cursor = doc.getCursor();
         var selection = doc.getSelection();
         var isEmpty = !selection;
+        var isWithBold = false;
 
         // if selection is empty, expend selection to detect a syntax
-        if (isEmpty && cursor.ch > 0) {
-            expendSelection();
+        if (isEmpty) {
+            if (cursor.ch > 2) {
+                expendWithBoldSelection();
+            }
+
+            if (isWithBold !== 'with' && cursor.ch > 1) {
+                expendOnlyBoldSelection();
+            }
+
+            if (!isWithBold && cursor.ch > 0) {
+                expendSelection();
+            }
         }
 
         var isRemoved = isNeedRemove();
@@ -57,9 +91,11 @@ var Italic = {
 
         doc.replaceSelection(result, 'around');
 
-        if (isEmpty && !isRemoved) {
+        if (isEmpty) {
             setCursorToCenter();
         }
+
+        cm.focus();
     },
     keyMap: ['Ctrl-I', 'Cmd-I']
 };
