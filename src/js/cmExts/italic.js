1692b5a4c0b68e7a30f96b8f5974acc2e589f0e5
신승엽/협업시스템개발팀 <flysky@nhnent.com>
2015-03-19 14:30:55 +0900

    selection이 없을 때 itatlic 동작

--- src/js/cmExts/italic.js
+++ src/js/cmExts/italic.js
@@ -2,7 +2,7 @@
 
 var italicRegex = /^[\*_][^\*_]*[\*_]$/;
 var boldItalicRegex = /^[\*_]{3,}[^\*_]*[\*_]{3,}$/;
-var Bold = {
+var Italic = {
     name: 'Italic',
     type: 'md',
     fn: function bold(cm) {
@@ -10,28 +10,58 @@ var Bold = {
             return CodeMirror.Pass;
         }
 
-        function isNeedRemove(text) {
-            return italicRegex.test(text) || boldItalicRegex.test(text);
+        cm.execCommand('singleSelection');
+
+        function isNeedRemove() {
+            return italicRegex.test(selection) || boldItalicRegex.test(selection);
         }
 
-        function append(text) {
-            return '*' + text + '*';
+        function append() {
+            return '*' + selection + '*';
         }
 
-        function remove(text) {
-            return text.substr(1, text.length - 2);
+        function remove() {
+            return selection.substr(1, selection.length - 2);
+        }
+
+        function expendSelection() {
+            doc.setSelection({line: cursor.line, ch: cursor.ch - 1}, {line: cursor.line, ch: cursor.ch + 1});
+
+            var tmpSelection = doc.getSelection();
+            if (tmpSelection === '**' || tmpSelection === '__') {
+                selection = tmpSelection;
+            } else {
+                doc.setSelection(cursor);
+            }
+        }
+
+        function setCursorToCenter() {
+            doc.setCursor(cursor.line, cursor.ch + 1);
         }
 
         var doc = cm.getDoc();
+        var cursor = doc.getCursor();
         var selection = doc.getSelection();
-        var result = isNeedRemove(selection) ? remove(selection) : append(selection);
+        var isEmpty = !selection;
+
+        // if selection is empty, expend selection to detect a syntax
+        if (isEmpty && cursor.ch > 0) {
+            expendSelection();
+        }
+
+        var isRemoved = isNeedRemove();
+        var result = isRemoved ? remove() : append();
 
         console.log(selection);
         console.log(result);
 
         doc.replaceSelection(result, 'around');
+
+        if (isEmpty && !isRemoved) {
+            setCursorToCenter();
+        }
     },
     keyMap: ['Ctrl-I', 'Cmd-I']
 };
 
-module.exports = Bold;
+module.exports = Italic;
