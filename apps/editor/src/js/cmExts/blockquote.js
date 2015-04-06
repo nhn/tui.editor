3583796e133d5d3af145570db7b4700d6e0b56ab
Sungho Kim <sungho-kim@nhnent.com>
2015-04-06 15:29:44 +0900

    blockquote생성

---
+++ apps/editor/src/js/cmExts/blockquote.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var CodeMirror = window.CodeMirror;
+
+var Blockquote = {
+    name: 'Blockquote',
+    type: 'md',
+    fn: function blockquote(cm) {
+        
+    },
+    keyMap: ['Ctrl-Q', 'Cmd-Q']
+};
+
+module.exports = Blockquote;
