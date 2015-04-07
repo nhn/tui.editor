f5f3ee311db86d1d630c6a2899b72034429ca075
Sungho Kim <sungho-kim@nhnent.com>
2015-04-07 18:57:55 +0900

    라인 커맨드 작업 resolve #39

---
+++ test/cmExts/hr.spec.js
@@ -0,0 +1,47 @@
+var HR = require('../../src/js/cmExts/hr');
+
+var CodeMirror = window.CodeMirror;
+
+describe('HR', function() {
+    'use strict';
+
+    var cm;
+
+    beforeEach(function() {
+        var textArea = $('<textarea />'),
+            sourceText;
+
+        $('body').append(textArea);
+
+        cm = CodeMirror.fromTextArea(textArea[0], {
+            lineWrapping: true,
+            mode: 'gfm',
+            theme: 'default',
+            dragDrop: false
+        });
+
+        sourceText = ['mytext1', '', 'mytext2', 'mytext3'];
+
+        cm.setValue(sourceText.join('\n'));
+    });
+
+    describe('현재라인 바로 밑에 HR 라인을 추가한다', function() {
+        it('현재라인 밑에 ***가 추가되었다', function() {
+            cm.setCursor(2, 3);
+
+            HR.fn(cm);
+
+            expect(cm.getValue()).toEqual(['mytext1', '', 'mytext2', '***', 'mytext3'].join('\n'));
+        });
+    });
+
+    describe('셀렉션이 있는경우 셀렉션의 내용을 라인으로 대체한다', function() {
+        it('셀렉션 영역이 ***로 대체되었다', function() {
+            cm.setSelection({line: 0, ch: 1}, {line: 2, ch: 2});
+
+            HR.fn(cm);
+
+            expect(cm.getValue()).toEqual(['m***text2', 'mytext3'].join('\n'));
+        });
+    });
+});
