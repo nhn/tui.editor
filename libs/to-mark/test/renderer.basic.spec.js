2ceb28977680f06f90ac98b6819bce80d4dd1562
Sungho Kim <sungho-kim@nhnent.com>
2015-05-28 17:01:03 +0900

    basicRender Heading resolve #4

--- libs/to-mark/test/renderer.basic.spec.js
+++ libs/to-mark/test/renderer.basic.spec.js
@@ -9,18 +9,72 @@ describe('basicRenderer', function() {
         result;
 
     describe('Headings', function() {
-        it('H1', function() {
+        it('heading with empty text', function() {
+            runner = new DomRunner(toDom('<h1></h1>'));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('# ');
+        });
+
+        it('heading with text', function() {
             runner = new DomRunner(toDom('<h1>heading</h1>'));
             runner.next();
             result = basicRenderer.convert(runner);
             expect(result).toEqual('# heading');
         });
 
-        it('H1 EM', function() {
-            runner = new DomRunner(toDom('<h1><em>heading</em></h1>'));
+        it('heading with inline element', function() {
+            var htmlStr = [
+                '<h1>',
+                    '<em>heading</em>',
+                '</h1>'
+            ];
+
+            runner = new DomRunner(toDom(htmlStr.join('')));
             runner.next();
             result = basicRenderer.convert(runner);
             expect(result).toEqual('# *heading*');
+        });
+
+        it('H1 ~ H6', function() {
+            var htmlStr = [
+                '<h1>1</h1>',
+                '<h2>2</h2>',
+                '<h3>3</h3>',
+                '<h4>4</h4>',
+                '<h5>5</h5>',
+                '<h6>6</h6>'
+            ];
+
+            runner = new DomRunner(toDom(htmlStr[0]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('# 1');
+
+            runner = new DomRunner(toDom(htmlStr[1]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('## 2');
+
+            runner = new DomRunner(toDom(htmlStr[2]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('### 3');
+
+            runner = new DomRunner(toDom(htmlStr[3]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('#### 4');
+
+            runner = new DomRunner(toDom(htmlStr[4]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('##### 5');
+
+            runner = new DomRunner(toDom(htmlStr[5]));
+            runner.next();
+            result = basicRenderer.convert(runner);
+            expect(result).toEqual('###### 6');
         });
     });
 });
--- libs/to-mark/test/toDom.spec.js
+++ libs/to-mark/test/toDom.spec.js
@@ -6,20 +6,22 @@ describe('toDom', function() {
     describe('convert block tag to dom', function() {
         it('node is correct', function() {
             var dom = toDom('<h1>Hello World!</h1>');
-            expect(dom.childNodes[0].tagName).toEqual('H1');
-            expect(dom.childNodes[0].innerText).toEqual('Hello World!');
+            var node = dom.childNodes[0];
+            var nodeText = node.innerText || node.textContent;
+
+            expect(node.tagName).toEqual('H1');
+            expect(nodeText).toEqual('Hello World!');
         });
     });
 
     describe('convert inline tag string to dom', function() {
         it('node is correct', function() {
             var dom = toDom('<img src="https://www.google.co.kr/images/nav_logo195.png" alt="altText" />');
-            expect(dom.childNodes.length).toEqual(1);
-            expect(dom.childNodes[0].tagName).toEqual('IMG');
-            expect(dom.childNodes[0].attributes[0].name).toEqual('src');
-            expect(dom.childNodes[0].src).toEqual('https://www.google.co.kr/images/nav_logo195.png');
-            expect(dom.childNodes[0].attributes[1].name).toEqual('alt');
-            expect(dom.childNodes[0].alt).toEqual('altText');
+            var node = dom.childNodes[0];
+
+            expect(node.tagName).toEqual('IMG');
+            expect(node.src).toEqual('https://www.google.co.kr/images/nav_logo195.png');
+            expect(node.alt).toEqual('altText');
         });
     });
 });
