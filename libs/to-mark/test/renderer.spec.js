4efaabc10f9b09ff67e106093c6f6c2d6656f26f
Sungho Kim <sungho-kim@nhnent.com>
2015-05-28 16:55:11 +0900

    fixed render comma rule bug

--- libs/to-mark/test/renderer.spec.js
+++ libs/to-mark/test/renderer.spec.js
@@ -7,43 +7,63 @@ var Renderer = require('../src/renderer'),
 describe('renderer', function() {
     var runner;
 
-    beforeEach(function() {
-        runner = new DomRunner(toDom('<h1>test</h1>'));
-        runner.next();
-    });
-
     it('can take the rule to render', function() {
         var convertedText,
             renderer = Renderer.factory();
 
         renderer.addRule('H1, H2, H3, H4, H5, H6', function() {
-            return 'mytext';
+            return 'markdownText';
         });
+
+        runner = new DomRunner(toDom('<h1>test</h1>'));
+        runner.next();
 
         convertedText = renderer.convert(runner);
 
-        expect(convertedText).toEqual('mytext');
+        expect(convertedText).toEqual('markdownText');
     });
 
     it('add rules with factory', function() {
         var convertedText,
             renderer = Renderer.factory({
                 'H1, H2, H3, H4, H5, H6': function() {
-                    return 'mytext';
+                    return 'markdownText';
                 }
             });
 
+        runner = new DomRunner(toDom('<h1>test</h1>'));
+        runner.next();
+
         convertedText = renderer.convert(runner);
 
-        expect(convertedText).toEqual('mytext');
+        expect(convertedText).toEqual('markdownText');
     });
 
     it('if there is no rule, conveter returns undefined', function() {
         var convertedText,
             renderer = Renderer.factory();
 
+        runner = new DomRunner(toDom('<h1>test</h1>'));
+        runner.next();
+
         convertedText = renderer.convert(runner);
 
         expect(convertedText).toBeUndefined();
+    });
+
+    it('rules are can be assigned separately with comma', function() {
+        var convertedText,
+            renderer = Renderer.factory({
+                'H1, H2, H3, H4, H5, H6': function() {
+                    return 'markdownText';
+                }
+            });
+
+        runner = new DomRunner(toDom('<h2>test</h2>'));
+        runner.next();
+
+        convertedText = renderer.convert(runner);
+
+        expect(convertedText).toEqual('markdownText');
     });
 });
