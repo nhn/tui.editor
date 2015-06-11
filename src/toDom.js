79231d59e039f689a4884233d0b32e35a2a1ecf7
Sungho Kim <sungho-kim@nhnent.com>
2015-06-11 16:58:14 +0900

    Convert with dom node resolve #25

--- src/toDom.js
+++ src/toDom.js
@@ -12,15 +12,22 @@
  * @return {DOMElement[]} dom element
  */
 function toDom(html) {
-    var wrapper = document.createElement('div');
+    var wrapper;
 
-    //trim text
-    html = html.replace(/^[\s]+|[\s]+$|[\r\n\t]/g, '');
+    if (Object.prototype.toString.call(html) === '[object String]') {
+        wrapper = document.createElement('div');
 
-    //remove spaces more than 1(if need more space, must use &nbsp)
-    html = html.replace(/[\s]+/g, ' ');
+        //trim text
+        html = html.replace(/^[\s]+|[\s]+$|[\r\n\t]/g, '');
 
-    wrapper.innerHTML = html;
+        //remove spaces more than 1(if need more space, must use &nbsp)
+        html = html.replace(/[\s]+/g, ' ');
+
+        wrapper.innerHTML = html;
+    } else {
+        wrapper = html;
+    }
+
     wrapper.__htmlRootByToMark = true;
 
     return wrapper;
