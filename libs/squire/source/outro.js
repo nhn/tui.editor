4fab4495ee27409a85cd27d0a284fe98766452b4
Neil Jenkins <neil@nmjenkins.com>
2015-06-23 15:16:08 +0700

    Merge pull request #103 from shiawuen/top
    fix bug on Browserify component when embed in multi layers of iframe

--- libs/squire/source/outro.js
+++ libs/squire/source/outro.js
@@ -1,14 +1,14 @@
 /*jshint ignore:start */
 
-if ( top !== win ) {
-    win.editor = new Squire( doc );
-    if ( win.onEditorLoad ) {
-        win.onEditorLoad( win.editor );
-        win.onEditorLoad = null;
-    }
+if ( typeof exports === 'object' ) {
+    module.exports = Squire;
 } else {
-    if ( typeof exports === 'object' ) {
-        module.exports = Squire;
+    if ( top !== win ) {
+        win.editor = new Squire( doc );
+        if ( win.onEditorLoad ) {
+            win.onEditorLoad( win.editor );
+            win.onEditorLoad = null;
+        }
     } else {
         win.Squire = Squire;
     }
