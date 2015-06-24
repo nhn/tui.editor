24495904fde3529a68883aac95d0eb2f41bc4b8e
Sungho Kim <sungho-kim@nhnent.com>
2015-06-24 12:12:14 +0900

    amd considered entry point

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@
 
 var toMark = require('./toMark');
 
-window.toMark = toMark;
+if (typeof define === 'function' && define.amd) {
+    define(function() {
+        return toMark;
+    });
+} else if (typeof exports !== 'undefined') {
+    module.exports = toMark;
+}
+
+if (typeof window !== 'undefined') {
+    window.toMark = toMark;
+}
