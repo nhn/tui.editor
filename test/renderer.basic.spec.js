ba443e345ba28dbf984e4f06de4f2aa01ed95bd2
Sungho Kim <sungho-kim@nhnent.com>
2015-07-08 14:40:47 +0900

    process br in end of li

--- test/renderer.basic.spec.js
+++ test/renderer.basic.spec.js
@@ -161,8 +161,12 @@ describe('basicRenderer', function() {
             expect(getMarkdownText('<ol><li></li></ol>', '', 2)).toEqual('1.  \n');
         });
 
-        it('return in li from nest list', function() {
+        it('return between li text content from nest list', function() {
             expect(getMarkdownText('<li><ul></ul></li>', '* text  \ntext\n* text', 2)).toEqual('\n    * text  \n    text\n    * text');
+        });
+
+        it('return end of li from nest list', function() {
+            expect(getMarkdownText('<li><ul></ul></li>', '* text  \n\n* text', 2)).toEqual('\n    * text\n    * text');
         });
     });
 
