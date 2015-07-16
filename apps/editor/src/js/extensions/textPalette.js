b2aa87e3f61eb2df47aeaf9a79debcb148dd2dfd
Sungho Kim <sungho-kim@nhnent.com>
2015-07-16 14:03:30 +0900

    completeHint renamed to textPalette

--- apps/editor/src/js/extensions/textPalette.js
+++ apps/editor/src/js/extensions/textPalette.js
@@ -2,21 +2,28 @@
 
 var extManager = require('../extManager');
 
-extManager.defineExtension('completeHint', function(editor) {
+extManager.defineExtension('textPalette', function(editor) {
     var $layer = $('<div style="z-index:9999"><input type="text" style="background:white" /></div>');
+    var triggers = editor.options.textPalette.triggers,
+        querySender = editor.options.textPalette.querySender;
 
     $(editor.options.el).append($layer);
 
     $layer.find('input').on('keyup', function(e) {
+        var query = $layer.find('input').val();
+
         if (e.which === 13) {
+            editor.getCurrentModeEditor().replaceSelection(query);
             hideUI($layer);
-            editor.getCurrentModeEditor().replaceSelection('awefawefweaf');
+        } else {
+            querySender(query, function(list) {
+                updateUI($layer, list);
+            });
         }
     });
 
     editor.eventManager.listen('change', function(ev) {
-        if (ev.textContent[ev.caretOffset] === '@') {
-            console.log(ev);
+        if (triggers.indexOf(ev.textContent[ev.caretOffset]) !== -1) {
             editor.addWidget(ev.selection, $layer[0], 'over');
             showUI($layer);
         }
@@ -31,4 +38,8 @@ function showUI($layer) {
 function hideUI($layer) {
     $layer.hide();
     $layer.find('input').val('');
+}
+
+function updateUI($layer, list) {
+    console.log(list);
 }
