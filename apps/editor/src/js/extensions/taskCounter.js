8890f54e39c79620d48f7d69c12165f3d1b5a544
Sungho Kim <shirenbeat@gmail.com>
2015-08-03 17:42:34 +0900

    fix lint result

--- apps/editor/src/js/extensions/taskCounter.js
+++ apps/editor/src/js/extensions/taskCounter.js
@@ -7,8 +7,10 @@ var FIND_CHECKED_TASK_RX = /^\s*\* \[[xX]\] [^\n]*/mg;
 
 extManager.defineExtension('taskCounter', function(editor) {
     editor.getTaskCount = function() {
-        if(editor.isMarkdownMode()) {
-            var found = editor.mdEditor.getValue().match(FIND_TASK_RX);
+        var found;
+
+        if (editor.isMarkdownMode()) {
+            found = editor.mdEditor.getValue().match(FIND_TASK_RX);
             return found ? found.length : 0;
         } else {
             return editor.wwEditor.get$Body().find('input').length;
@@ -16,8 +18,10 @@ extManager.defineExtension('taskCounter', function(editor) {
     }
 
     editor.getCheckedTaskCount = function() {
-        if(editor.isMarkdownMode()) {
-            var found = editor.mdEditor.getValue().match(FIND_CHECKED_TASK_RX);
+        var found;
+
+        if (editor.isMarkdownMode()) {
+            found = editor.mdEditor.getValue().match(FIND_CHECKED_TASK_RX);
             return found ? found.length : 0;
         } else {
             return editor.wwEditor.get$Body().find('input:checked').length;
