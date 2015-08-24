01782db8c6d1a1d597e1d5b3e3eb60cba45f1ffd
Sungho Kim <shirenbeat@gmail.com>
2015-08-24 20:45:50 +0900

    enhance about return resolve #69, resolve #68

--- src/toMark.js
+++ src/toMark.js
@@ -11,7 +11,7 @@ var DomRunner = require('./domRunner'),
     gfmRenderer = require('./renderer.gfm');
 
 var FIND_FIRST_LAST_WITH_SPACE_RETURNS_RX = /^[\n]+|[\s\n]+$/g,
-    FIND_TRIPLE_RETURNS_RX = /\n\n\n/g,
+    FIND_TRIPLE_OVER_RETURNS_RX = /(\n){3,}/g,
     FIND_RETURNS_RX = /([ \xA0]){2,}\n/g,
     FIND_EMPTYLINE_WITH_RETURN_RX = /\n[ \xA0]+\n\n/g,
     FIND_MULTIPLE_EMPTYLINE_BETWEEN_BLOCK_RX = /(\n\n)?([ \xA0]+\n){2,}/g,
@@ -81,28 +81,43 @@ function parse(runner, renderer) {
  * @return {string} result
  */
 function finalize(text, isGfm) {
+    //console.log(JSON.stringify(text));
     //collapse duplicated returns made by <br /> and block element
+    //블럭요소가 만들어내는 개행두개와 겹친 BR에 대한 처리
     text = text.replace(FIND_DUPLICATED_2_RETURNS_WITH_BR_RX, '\n\n');
+    //console.log(JSON.stringify(text));
 
     //collpase emptyline and additional return
+    //빈라인과 개행이 함게 있다면 개행 하나로 처리
     text = text.replace(FIND_EMPTYLINE_WITH_RETURN_RX, '\n  \n');
+    //console.log(JSON.stringify(text));
 
     //collapse return and <br>
+    //개행과 br이 함께 있다면 개행하나로 처리
     text = text.replace(FIND_DUPLICATED_RETURN_WITH_BR_RX, '\n');
+    //console.log(JSON.stringify(text));
 
-    //collapse triple returns made by consecutive block elements
-    text = text.replace(FIND_TRIPLE_RETURNS_RX, '\n\n');
+    //collapse over triple returns made by consecutive block elements
+    //개행이 여러개 있다면 한개의 빈라인으로 대체
+    text = text.replace(FIND_TRIPLE_OVER_RETURNS_RX, '\n\n');
+    //console.log(JSON.stringify(text));
 
     //remove multi empty lines between block
+    //빈라인이 여러개 있다면 한개로 대체
     text = text.replace(FIND_MULTIPLE_EMPTYLINE_BETWEEN_BLOCK_RX, '\n\n');
+    //console.log(JSON.stringify(text));
 
     //remove first and last \n
+    //시작과 마지막 개행제거
     text = text.replace(FIND_FIRST_LAST_WITH_SPACE_RETURNS_RX, '');
+    //console.log(JSON.stringify(text));
 
     //in gfm replace '  \n' make by <br> to '\n'
+    //gfm모드인경우 임의 개행에 스페이스를 제거
     if (isGfm) {
         text = text.replace(FIND_RETURNS_RX, '\n');
     }
+    //console.log(JSON.stringify(text));
 
     return text;
 }
