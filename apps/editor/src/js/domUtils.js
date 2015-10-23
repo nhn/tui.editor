e069625f10131f2c87d7a06f352708f67292670c
Sungho Kim <shirenbeat@gmail.com>
2015-10-23 17:04:09 +0900

    add JsDoc to domutils

--- apps/editor/src/js/domUtils.js
+++ apps/editor/src/js/domUtils.js
@@ -6,24 +6,44 @@
 'use strict';
 
 /**
- * domUtils
- * @exports domUtils
+ * isTextNode
+ * Check if node is text node
+ * @param {Node} node node to check
+ * @return {boolean} result
  */
-
 var isTextNode = function(node) {
     return node && node.nodeType === Node.TEXT_NODE;
 };
 
+/**
+ * isElemNode
+ * Check if node is element node
+ * @param {Node} node node to check
+ * @return {boolean} result
+ */
 var isElemNode = function(node) {
     return node && node.nodeType === Node.ELEMENT_NODE;
 };
 
+/**
+ * getChildNodeAt
+ * Get child node in given parent and index
+ * @param {HTMLElement} elem parent element
+ * @param {number} index node index
+ * @return {Node} child
+ */
 var getChildNodeAt = function(elem, index) {
     if (elem.childNodes.length && index >= 0) {
         return elem.childNodes[index];
     }
 };
 
+/**
+ * getNodeName
+ * Get node name of node
+ * @param {Node} node node
+ * @return {string} node name
+ */
 var getNodeName = function(node) {
     if (isElemNode(node)) {
         return node.tagName;
@@ -32,6 +52,12 @@ var getNodeName = function(node) {
     }
 };
 
+/**
+ * getTextLength
+ * Get node offset length of node(for Range API)
+ * @param {Node} node node
+ * @return {number} length
+ */
 var getTextLength = function(node) {
     var len;
 
@@ -44,6 +70,12 @@ var getTextLength = function(node) {
     return len;
 };
 
+/**
+ * getOffsetLength
+ * Get node offset length of node(for Range API)
+ * @param {Node} node node
+ * @return {number} length
+ */
 var getOffsetLength = function(node) {
     var len;
 
