28b5ea5c6ed82c88139dc426dacfd307db4d4442
Sungho Kim <shirenbeat@gmail.com>
2015-12-28 16:57:44 +0900

    task dont break color span fixed #400

--- src/js/wwSelectionMarker.js
+++ src/js/wwSelectionMarker.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+var domUtils = require('./domUtils');
+
 var MARKER_CSS_CLASS = 'tui-editor-selection-marker';
 
 /**
@@ -52,12 +54,22 @@ WwSelectionMarker.prototype._makeMarker = function(sq) {
 WwSelectionMarker.prototype.restore = function(sq) {
     var newRange = sq.getSelection().cloneRange();
 
-    newRange.setStartAfter(this._markerNode);
+    newRange.setStartBefore(this._markerNode);
     newRange.collapse(true);
 
     sq.setSelection(newRange);
 
     $(this._markerNode).remove();
+
+    //task안의 컬러의 경우, 컨테이너가 컬러span 그리고 startOffset이 0인경우 컬러가 끊긴다.
+    //입력시 컬러가 이어지도록하려면 추가 보정이 필요하다
+    if (newRange.startOffset === 0
+        && domUtils.isTextNode(newRange.startContainer.childNodes[0])
+        && !newRange.startContainer.childNodes[0].textContent.replace(/\u200B/, '')
+    ) {
+        newRange.setStart(newRange.startContainer.childNodes[0], 1);
+        sq.setSelection(newRange);
+    }
 
     return newRange;
 };
