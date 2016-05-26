7f0f6fa15e26c0bdf762528005aa21ccba932c26
Neil Jenkins <neil@nmjenkins.com>
2016-05-26 13:09:19 +1000

    Strip HTML surrounding copied fragment added by Edge.
    Fixes #181

--- libs/squire/build/squire-raw.js
+++ libs/squire/build/squire-raw.js
@@ -3837,9 +3837,23 @@ var addLinks = function ( frag, root, self ) {
 // insertTreeFragmentIntoRange will delete the selection so that it is replaced
 // by the html being inserted.
 proto.insertHTML = function ( html, isPaste ) {
-    var range = this.getSelection(),
-        frag = this._doc.createDocumentFragment(),
-        div = this.createElement( 'DIV' );
+    var range = this.getSelection();
+    var frag = this._doc.createDocumentFragment();
+    var div = this.createElement( 'DIV' );
+    var startFragmentIndex, endFragmentIndex;
+    var root, node, event;
+
+    // Edge doesn't just copy the fragment, but includes the surrounding guff
+    // including the full <head> of the page. Need to strip this out. In the
+    // future should probably run all pastes through DOMPurify, but this will
+    // do for now
+    if ( isPaste ) {
+        startFragmentIndex = html.indexOf( '<!--StartFragment-->' );
+        endFragmentIndex = html.lastIndexOf( '<!--EndFragment-->' );
+        if ( startFragmentIndex > -1 && endFragmentIndex > -1 ) {
+            html = html.slice( startFragmentIndex + 20, endFragmentIndex );
+        }
+    }
 
     // Parse HTML into DOM tree
     div.innerHTML = html;
@@ -3849,9 +3863,9 @@ proto.insertHTML = function ( html, isPaste ) {
     this.saveUndoState( range );
 
     try {
-        var root = this._root;
-        var node = frag;
-        var event = {
+        root = this._root;
+        node = frag;
+        event = {
             fragment: frag,
             preventDefault: function () {
                 this.defaultPrevented = true;
