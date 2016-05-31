0de77bf84b56a2dd51d8277e98954a8c72fee529
junghwan-park <junghwan.park@nhnent.com>
2016-05-31 17:25:35 +0900

    add T/C for condition check - current cursor position in ListItem

--- test/wysiwygCommands/decreaseDepth.spec.js
+++ test/wysiwygCommands/decreaseDepth.spec.js
@@ -2,6 +2,7 @@
 
 var DecreaseDepth = require('../../src/js/wysiwygCommands/decreaseDepth'),
     WysiwygEditor = require('../../src/js/wysiwygEditor'),
+    WwTaskManager = require('../../src/js/wwTaskManager'),
     EventManager = require('../../src/js/eventManager');
 
 describe('DecreaseDepth', function() {
@@ -17,6 +18,17 @@ describe('DecreaseDepth', function() {
         wwe.init();
 
         sq = wwe.getEditor();
+        wwe.addManager(WwTaskManager);
+
+        wwe.get$Body().html([
+            '<ul>',
+            '<li class="task-list-item"><div><input type="checkbox"> abcdef</div>',
+            '<ul>',
+            '<li class="task-list-item"><div><input type="checkbox"> abcde</div></li>',
+            '</ul></li>',
+            '<li class="task-list-item"><div><input type="checkbox"> </div></li>',
+            '</ul>'
+        ].join(''));
     });
 
     //we need to wait squire input event process
@@ -27,17 +39,8 @@ describe('DecreaseDepth', function() {
         });
     });
 
-    it('success when it not first li.', function() {
+    it('should be able to decrease depth second to first.', function() {
         var range = wwe.getEditor().getSelection().cloneRange();
-        wwe.get$Body().html([
-            '<ul>',
-            '<li class="task-list-item"><div><input type="checkbox"> abcdef</div>',
-            '<ul>',
-            '<li class="task-list-item"><div><input type="checkbox"> abcde</div></li>',
-            '</ul></li>',
-            '<li class="task-list-item"><div><input type="checkbox"> </div></li>',
-            '</ul>'
-        ].join(''));
 
         range.setStartAfter(wwe.get$Body().find('input')[1]);
         range.collapse(true);
@@ -45,6 +48,16 @@ describe('DecreaseDepth', function() {
         sq.setSelection(range);
 
         DecreaseDepth.exec(wwe);
+
+        expect(sq.get$Body().find('ul li').length).toEqual(3);
+        expect(sq.get$Body().find('ul li ul').length).toEqual(0);
+        expect(sq.get$Body().find('ul li ul li').length).toEqual(0);
+        expect(sq.get$Body().find('ul li input').length).toEqual(3);
+        expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+    });
+    it('should break out list element and delete input.', function() {
+        var range = wwe.getEditor().getSelection().cloneRange();
+
         range.setStartAfter(wwe.get$Body().find('input')[2]);
         range.collapse(true);
 
@@ -53,33 +66,89 @@ describe('DecreaseDepth', function() {
         DecreaseDepth.exec(wwe);
 
         expect(sq.get$Body().find('ul li').length).toEqual(2);
-        expect(sq.get$Body().find('ul li ul').length).toEqual(0);
-        expect(sq.get$Body().find('ul li ul li').length).toEqual(0);
-        expect(sq.get$Body().find('ul li input').length).toEqual(2);
-        expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
-    });
-    it('fail when it does not have previous li.', function() {
-        var range = wwe.getEditor().getSelection().cloneRange();
-        wwe.get$Body().html([
-            '<ul>',
-            '<li class="task-list-item"><div><input type="checkbox"> abcdef</div>',
-            '<ul>',
-            '<li class="task-list-item"><div><input type="checkbox"> abcde</div></li>',
-            '</ul></li>',
-            '<li class="task-list-item"><div><input type="checkbox"> </div></li>',
-            '</ul>'
-        ].join(''));
-
-        range.setStartAfter(wwe.get$Body().find('input')[0]);
-        range.collapse(true);
-
-        sq.setSelection(range);
-
-        DecreaseDepth.exec(wwe);
-
-        expect(sq.get$Body().find('ul li').length).toEqual(3);
+        expect(sq.get$Body().find('ul li ul').length).toEqual(1);
         expect(sq.get$Body().find('ul li ul li').length).toEqual(1);
         expect(sq.get$Body().find('ul li input').length).toEqual(2);
         expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+    });
+    describe('should decrease depth when cursor', function() {
+        it('at startOffset 0.', function() {
+            var range = wwe.getEditor().getSelection().cloneRange();
+
+            range.setStart(wwe.get$Body().find('input')[1].nextSibling, 0);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+
+            expect(sq.get$Body().find('ul li').length).toEqual(3);
+            expect(sq.get$Body().find('ul li ul').length).toEqual(0);
+            expect(sq.get$Body().find('ul li ul li').length).toEqual(0);
+            expect(sq.get$Body().find('ul li input').length).toEqual(3);
+            expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+        });
+        it('should decrease depth when cursor at startOffset 1.', function() {
+            var range = wwe.getEditor().getSelection().cloneRange();
+
+            range.setStart(wwe.get$Body().find('input')[1].nextSibling, 1);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+
+            expect(sq.get$Body().find('ul li').length).toEqual(3);
+            expect(sq.get$Body().find('ul li ul').length).toEqual(0);
+            expect(sq.get$Body().find('ul li ul li').length).toEqual(0);
+            expect(sq.get$Body().find('ul li input').length).toEqual(3);
+            expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+        });
+        it('should decrease depth when cursor before input element.', function() {
+            var range = wwe.getEditor().getSelection().cloneRange();
+
+            range.setStartBefore(wwe.get$Body().find('input')[1]);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+
+            expect(sq.get$Body().find('ul li').length).toEqual(3);
+            expect(sq.get$Body().find('ul li ul').length).toEqual(0);
+            expect(sq.get$Body().find('ul li ul li').length).toEqual(0);
+            expect(sq.get$Body().find('ul li input').length).toEqual(3);
+            expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+        });
+    });
+    describe('should not decrease depth when', function() {
+        it('cursor at 2 or more.', function() {
+            var range = wwe.getEditor().getSelection().cloneRange();
+
+            range.setStart(wwe.get$Body().find('input')[1].nextSibling, 2);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+            range.setStart(wwe.get$Body().find('input')[1].nextSibling, 3);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+            range.setStart(wwe.get$Body().find('input')[1].nextSibling, 4);
+            range.collapse(true);
+
+            sq.setSelection(range);
+
+            DecreaseDepth.exec(wwe);
+
+            expect(sq.get$Body().find('ul li').length).toEqual(3);
+            expect(sq.get$Body().find('ul li ul').length).toEqual(1);
+            expect(sq.get$Body().find('ul li ul li').length).toEqual(1);
+            expect(sq.get$Body().find('ul li input').length).toEqual(3);
+            expect(sq.get$Body().find('ul li').hasClass('task-list-item')).toBe(true);
+        });
     });
 });
