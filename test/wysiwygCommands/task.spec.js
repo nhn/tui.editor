bf37f4b3ebafa2587e055a1afa265e21488f9ac2
junghwan-park <junghwan.park@nhnent.com>
2016-06-03 14:55:04 +0900

    change task method of task creation

--- test/wysiwygCommands/task.spec.js
+++ test/wysiwygCommands/task.spec.js
@@ -43,11 +43,16 @@ describe('Task', function() {
     it('if already in empty task, dont do anything', function() {
         var range = sq.getSelection().cloneRange();
 
-        sq.setHTML('<ul><li class="task-list-item"><input type="checkbox"> text</li></ul>');
+        sq.setHTML('<ul><li><div>text</div></li></ul>');
 
         range.setStart(wwe.get$Body().find('li')[0], 1);
         range.collapse(true);
         sq.setSelection(range);
+
+        Task.exec(wwe);
+
+        expect(wwe.get$Body().find('li').length).toEqual(1);
+        expect(wwe.get$Body().find('ul input').length).toEqual(1);
 
         Task.exec(wwe);
 
