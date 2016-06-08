88c6686374a1b8869ad12e3de4d89ff958bdc95a
김성호/FE개발랩/NE <sungho-kim@nhnent.com>
2016-06-08 11:48:28 +0900

    Merge pull request #532 from fe/task-testcase-modify
    ww: IE10 Task.exec() 시 index error 해결

--- apps/editor/test/wysiwygCommands/task.spec.js
+++ apps/editor/test/wysiwygCommands/task.spec.js
@@ -43,11 +43,16 @@ describe('Task', function() {
     it('if already in empty task, dont do anything', function() {
         var range = sq.getSelection().cloneRange();
 
-        sq.setHTML('<ul><li class="task-list-item"><input type="checkbox"> text</li></ul>');
+        sq.setHTML('<div>text</div>');
 
-        range.setStart(wwe.get$Body().find('li')[0], 1);
+        range.setStart(wwe.get$Body().find('div')[0], 1);
         range.collapse(true);
         sq.setSelection(range);
+
+        Task.exec(wwe);
+
+        expect(wwe.get$Body().find('li').length).toEqual(1);
+        expect(wwe.get$Body().find('ul input').length).toEqual(1);
 
         Task.exec(wwe);
 
