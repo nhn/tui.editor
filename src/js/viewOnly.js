8933363613a6152e0de3a6366a3a0b3d255b61eb
Sungho Kim <shirenbeat@gmail.com>
2016-10-12 17:24:58 +0900

    viewOnly: deprecate getValue method resolved #793

--- src/js/viewOnly.js
+++ src/js/viewOnly.js
@@ -86,16 +86,6 @@ class ToastUIEditorViewOnly {
     }
 
     /**
-     * Get content of preview
-     * @api
-     * @memberOf ToastUIEditorViewOnly
-     * @returns {string}
-     */
-    getValue() {
-        return this.convertor.toMarkdown(this.preview.$el.find('div.tui-editor-contents').eq(0).html());
-    }
-
-    /**
      * Bind eventHandler to event type
      * @api
      * @memberOf ToastUIEditorViewOnly
