8691064a238820e4151fb995b6ef5137f652aad5
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2017-02-16 16:32:54 +0900

    Rollback commit 9a3e8ef(fix: code block does not wrap selected texts)

--- src/js/markdownCommands/codeBlock.js
+++ src/js/markdownCommands/codeBlock.js
@@ -23,28 +23,21 @@ const CodeBlock = CommandManager.command('markdown', /** @lends CodeBlock */{
     exec(mde) {
         const cm = mde.getEditor();
         const doc = cm.getDoc();
-        const range = mde.getCurrentRange();
-        const from = {
-            line: range.from.line,
-            ch: 0
-        };
-        const to = {
-            line: range.to.line,
-            ch: doc.getLineHandle(range.to.line).text.length
-        };
-        const textToModify = doc.getRange(from, to);
-        const textLinesToModify = textToModify.split('\n');
-        textLinesToModify.unshift('```');
-        textLinesToModify.push('```');
-        const lineLength = textLinesToModify.length;
-        for (let i = 0; i < lineLength; i += 1) {
-            textLinesToModify[i] = textLinesToModify[i];
+        let replaceText = '';
+        let rowFix;
+
+        const range = cm.getCursor();
+
+        if (doc.getLine(range.line).length) {
+            replaceText += '\n``` \n\n```\n\n';
+            doc.setCursor(range.line + 1, 0);
+            rowFix = 3;
+        } else {
+            replaceText += '\n``` \n\n```\n';
+            rowFix = 2;
         }
-        doc.replaceRange(textLinesToModify.join('\n'), from, to);
-
-        range.to.ch += 1;
-
-        doc.setCursor(range.to);
+        doc.replaceSelection(replaceText);
+        cm.setCursor(doc.getCursor().line - rowFix, 0);
 
         cm.focus();
     }
