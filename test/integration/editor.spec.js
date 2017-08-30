b634211f92f550eb130d1eafbdb15b315d244822
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2017-08-30 17:01:16 +0900

    fix: exception on section manager (fix #1006)

--- test/integration/editor.spec.js
+++ test/integration/editor.spec.js
@@ -33,4 +33,19 @@ describe('Editor', () => {
         expect(document.body.scrollTop).toBe(0);
         document.body.style.paddingTop = '0px';
     });
+
+    it('should not throw exception on initializing with long content in case of markdown type vertical scrollFollow enabled', done => {
+        // the exception will be thrown from async, we can't no.toThrow() here
+        // just wait and see if uncaught exception has been thrown or not
+        const editor = new Editor({ // eslint-disable-line
+            el: container,
+            height: '150px',
+            initialValue: 'a\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\n',
+            initialEditType: 'markdown',
+            previewStyle: 'vertical',
+            exts: ['scrollFollow']
+        });
+
+        setTimeout(done, 1000);
+    });
 });
