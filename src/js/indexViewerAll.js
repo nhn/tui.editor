bfbb10be27d79e0f3e5ba4c6f99b96ffb9505096
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2017-12-27 18:17:14 +0900

    refactor: remove mark and taskCounter ext from dist (fix #1053)

--- src/js/indexViewerAll.js
+++ src/js/indexViewerAll.js
@@ -5,7 +5,6 @@
 const EditorViewer = require('./indexViewer');
 
 import './extensions/chart/chart';
-import './extensions/mark/mark';
 import './extensions/table/table';
 import './extensions/colorSyntax';
 import './extensions/uml';
