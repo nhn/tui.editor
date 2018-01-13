6c674237e44607d368655f4337e1a400083a6027
Enrico Lamperti <sfroid+github@gmail.com>
2018-01-13 02:38:29 -0300

    feat: add Spanish language (close #33)

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ Depending on the type of use you want like production of *Markdown* or maybe to 
 * **Table**: Hate the Markdown table? You can do everything with a mouse
 
 ### And more
-* **i18n**: English, Dutch, Korean, Japanese, Chinese + language you extend.
+* **i18n**: English, Dutch, Korean, Japanese, Chinese, Spanish + language you extend.
 * **Viewer**: Renders Markdown content with extensions
 
 
---
+++ src/js/langs/es_ES.js
@@ -0,0 +1,50 @@
+/**
+* @fileoverview I18N for Spanish
+* @author Enrico Lamperti <oss@elamperti.com>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['es', 'es_ES'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Escribir',
+  'Preview': 'Vista previa',
+  'Headings': 'Encabezados',
+  'Paragraph': 'Párrafo',
+  'Bold': 'Negrita',
+  'Italic': 'Itálica',
+  'Strike': 'Tachado',
+  'Code': 'Código',
+  'Line': 'Línea',
+  'Blockquote': 'Cita',
+  'Unordered list': 'Lista desordenada',
+  'Ordered list': 'Lista ordenada',
+  'Task': 'Tarea',
+  'Insert link': 'Insertar enlace',
+  'Insert CodeBlock': 'Insertar bloque de código',
+  'Insert table': 'Insertar tabla',
+  'Insert image': 'Insertar imagen',
+  'Heading': 'Encabezado',
+  'Image URL': 'URL de la imagen',
+  'Select image file': 'Seleccionar archivo de imagen',
+  'Description': 'Descripción',
+  'OK': 'Aceptar',
+  'Cancel': 'Cancelar',
+  'File': 'Archivo',
+  'URL': 'URL',
+  'Link text': 'Texto del enlace',
+  'Add row': 'Agregar fila',
+  'Add col': 'Agregar columna',
+  'Remove row': 'Eliminar fila',
+  'Remove col': 'Eliminar columna',
+  'Align left': 'Alinear a la izquierda',
+  'Align center': 'Centrar',
+  'Align right': 'Alinear a la derecha',
+  'Remove table': 'Eliminar tabla',
+  'Would you like to paste as table?': '¿Desea pegar como tabla?',
+  'Text color': 'Color del texto',
+  'Auto scroll enabled': 'Desplazamiento automático habilitado',
+  'Auto scroll disabled': 'Desplazamiento automático deshabilitado',
+  'Cannot paste values ​​other than a table in the cell selection state': 'Sólo se pueden pegar tablas en el modo de selección de celdas',
+  'Choose language': 'Elegir idioma'
+});
