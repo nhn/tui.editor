0e2ad784d4bc494f9ad887364945c75fd4a0f517
Stepan Samko <stpnsamko@gmail.com>
2018-01-15 19:31:21 +0200

    feat: add Russian language

---
+++ src/js/langs/ru_RU.js
@@ -0,0 +1,51 @@
+/**
+* @fileoverview I18N for Russian
+* @author Stepan Samko <stpnsamko@gmail.com>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['ru', 'ru_RU'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Написать',
+  'Preview': 'Предварительный просмотр',
+  'Headings': 'Заголовки',
+  'Paragraph': 'Абзац',
+  'Bold': 'Жирный',
+  'Italic': 'Курсив',
+  'Strike': 'Зачеркнутый',
+  'Code': 'Встроенный код',
+  'Line': 'Строка',
+  'Blockquote': 'Блок цитирования',
+  'Unordered list': 'Неупорядоченный список',
+  'Ordered list': 'Упорядоченный список',
+  'Task': 'Задача',
+  'Insert link': 'Вставить ссылку',
+  'Insert CodeBlock': 'Вставить код',
+  'Insert table': 'Вставить таблицу',
+  'Insert image': 'Вставить изображение',
+  'Heading': 'Заголовок',
+  'Image URL': 'URL изображения',
+  'Select image file': 'Выбрать файл изображения',
+  'Description': 'Описание',
+  'OK': 'Хорошо',
+  'Cancel': 'Отмена',
+  'File': 'Файл',
+  'URL': 'URL',
+  'Link text': 'Текст ссылки',
+  'Add row': 'Добавить ряд',
+  'Add col': 'Добавить столбец',
+  'Remove row': 'Удалить ряд',
+  'Remove col': 'Удалить столбец',
+  'Align left': 'Выровнять по левому краю',
+  'Align center': 'Выровнять по центру',
+  'Align right': 'Выровнять по правому краю',
+  'Remove table': 'Удалить таблицу',
+  'Would you like to paste as table?': 'Вы хотите вставить в виде таблицы?',
+  'Text color': 'Цвет текста',
+  'Auto scroll enabled': 'Автоматическая прокрутка включена',
+  'Auto scroll disabled': 'Автоматическая прокрутка отключена',
+  'Cannot paste values ​​other than a table in the cell selection state':
+        'Вы не можете вставлять значения, отличные от таблицы, в состоянии выбора ячейки.',
+  'Choose language': 'Выбрать язык'
+});
