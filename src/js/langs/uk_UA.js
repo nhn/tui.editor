5d7c354a77ce21094095c38f05480096f99604c0
Nikolya <k_m_i@i.ua>
2018-02-01 09:22:19 +0200

    Ukrainian translation

---
+++ src/js/langs/uk_UA.js
@@ -0,0 +1,51 @@
+/**
+* @fileoverview I18N for Ukrainian
+* @author Nikolya <k_m_i@i.ua>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['uk', 'uk_UA'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Написати',
+  'Preview': 'Попередній перегляд',
+  'Headings': 'Заголовки',
+  'Paragraph': 'Абзац',
+  'Bold': 'Жирний',
+  'Italic': 'Курсив',
+  'Strike': 'Закреслений',
+  'Code': 'Вбудований код',
+  'Line': 'Лінія',
+  'Blockquote': 'Блок цитування',
+  'Unordered list': 'Невпорядкований список',
+  'Ordered list': 'Упорядкований список',
+  'Task': 'Завдання',
+  'Insert link': 'Вставити посилання',
+  'Insert CodeBlock': 'Вставити код',
+  'Insert table': 'Вставити таблицю',
+  'Insert image': 'Вставити зображення',
+  'Heading': 'Заголовок',
+  'Image URL': 'URL зображення',
+  'Select image file': 'Вибрати файл зображення',
+  'Description': 'Опис',
+  'OK': 'OK',
+  'Cancel': 'Скасувати',
+  'File': 'Файл',
+  'URL': 'URL',
+  'Link text': 'Текст посилання',
+  'Add row': 'Додати ряд',
+  'Add col': 'Додати стовпчик',
+  'Remove row': 'Видалити ряд',
+  'Remove col': 'Видалити стовпчик',
+  'Align left': 'Вирівняти по лівому краю',
+  'Align center': 'Вирівняти по центру',
+  'Align right': 'Вирівняти по правому краю',
+  'Remove table': 'Видалити таблицю',
+  'Would you like to paste as table?': 'Ви хочете вставити у вигляді таблиці?',
+  'Text color': 'Колір тексту',
+  'Auto scroll enabled': 'Автоматична прокрутка включена',
+  'Auto scroll disabled': 'Автоматична прокрутка відключена',
+  'Cannot paste values ​​other than a table in the cell selection state':
+        'Ви не можете вставляти значення, відмінні від таблиці, в стані вибору комірки.',
+  'Choose language': 'Вибрати мову'
+});
