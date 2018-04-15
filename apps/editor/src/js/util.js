68000ef68b79b51342f24ffbbe802e39bf0dfd6e
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2018-04-16 08:53:59 +0900

    feat: send imageping once in module load

--- apps/editor/src/js/util.js
+++ apps/editor/src/js/util.js
@@ -1,10 +1,17 @@
 import util from 'tui-code-snippet';
 
+let hostnameSent = false;
+
 /**
  * send host name
  * @ignore
  */
 function sendHostName() {
+  if (hostnameSent) {
+    return;
+  }
+  hostnameSent = true;
+
   const trackingID = 'UA-115377265-9';
   const applicationID = 'editor';
   const hitType = 'event';
