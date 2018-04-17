19ad758ebd45f0e828977a43f76200850082f876
KyuWoo Choi <kyuwoo.choi@gmail.com>
2018-04-17 09:46:23 +0900

    feat: add plantuml language for uml plugin (close #87) (#146)
    * feat: add plantuml language for uml plugin
    
    * refactor: apply code review (#146)

--- apps/editor/test/extensions/uml.spec.js
+++ apps/editor/test/extensions/uml.spec.js
@@ -25,6 +25,7 @@ describe('uml extension', () => {
   });
 
   it('create plant uml image in markdown preview', () => {
+    const lang = 'uml';
     editor = new TuiEditor({
       el: wrapper,
       previewStyle: 'vertical',
@@ -33,7 +34,24 @@ describe('uml extension', () => {
       exts: ['uml']
     });
 
-    editor.setValue(`\`\`\`uml\nAlice -> Bob: Hello\n\`\`\``);
+    editor.setValue(`\`\`\`${lang}\nAlice -> Bob: Hello\n\`\`\``);
+
+    jasmine.clock().tick(800);
+
+    expect(editor.preview.$el.get(0).querySelector('pre img').getAttribute('src')).toEqual('http://www.plantuml.com/plantuml/png/Syp9J4vLqBLJSCfFibBmICt9oUS20000');
+  });
+
+  it('create plant uml image for code block language plantuml', () => {
+    const lang = 'plantuml';
+    editor = new TuiEditor({
+      el: wrapper,
+      previewStyle: 'vertical',
+      height: '100px',
+      initialEditType: 'markdown',
+      exts: ['uml']
+    });
+
+    editor.setValue(`\`\`\`${lang}\nAlice -> Bob: Hello\n\`\`\``);
 
     jasmine.clock().tick(800);
 
