c715f411186c238c1d83e3a54f4642770a6be83d
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2018-04-30 17:31:52 +0900

    test: set testcafe and add some toolbar test

---
+++ test/e2e/markdown/toolbar/strike.spec.js
@@ -0,0 +1,31 @@
+import { Selector, ClientFunction } from 'testcafe';
+
+import editor from '../../editorFixture';
+
+fixture`markdown toolbar strike`
+  .page`http://localhost:8080/examples/example13-all-extension-blank.html`;
+
+test('click strike button then type text', async t => {
+  await t
+    .click(editor.toolbar.strike)
+    .pressKey('t')
+    .pressKey('e')
+    .pressKey('s')
+    .pressKey('t')
+    .expect(editor.markdown.lines.nth(0).textContent).eql('~~test~~')
+    .expect(editor.markdown.preview.innerHTML).contains('<s>');
+});
+
+test('type text, select the text then click strike button', async t => {
+  await t
+    .click(editor.markdown.lines.nth(0))
+    .pressKey('t')
+    .pressKey('e')
+    .pressKey('s')
+    .pressKey('t')
+    .pressKey('home')
+    .pressKey('shift+end')
+    .click(editor.toolbar.strike)
+    .expect(editor.markdown.lines.nth(0).textContent).eql('~~test~~')
+    .expect(editor.markdown.preview.innerHTML).contains('<s>');
+});
