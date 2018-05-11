9605b013fb7b932651de6327d62fb7250646d5bf
KyuWoo Choi <kyuwoo.choi@gmail.com>
2018-05-11 16:14:38 +0900

    fix: broken unit test caused by wrong spec path (#179)

--- test/unit/test.bundle.js
+++ test/unit/test.bundle.js
@@ -2,6 +2,6 @@
  * @fileoverview test bundle
  * @author NHN Ent. FE Development Lab <dl_javascript@nhnent.com>
  */
-const testsContext = require.context('..', true, /.+\.spec\.js?$/);
+const testsContext = require.context('.', true, /.+\.spec\.js?$/);
 
 testsContext.keys().forEach(testsContext);
