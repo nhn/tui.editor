1235d007974f3f19ef25c0341663e56b536b097f
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2018-06-12 16:07:05 +0900

    fix: new line after links make wrong additional BRs

--- test/e2e/editorFixture.js
+++ test/e2e/editorFixture.js
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import {Selector, t} from 'testcafe';
 
 class Toolbar {
   constructor() {
@@ -25,6 +25,29 @@ class Toolbar {
   }
 }
 
+class Tabbar {
+  constructor() {
+    const tabbar = Selector('.tui-editor-defaultUI > .te-mode-switch-section');
+
+    this.markdown = tabbar.find('.markdown');
+    this.wysiwyg = tabbar.find('.wysiwyg');
+  }
+}
+
+class Popup {
+  constructor() {
+    const link = Selector('.te-popup-add-link');
+
+    this.linkText = link.find('.te-link-text-input');
+    this.linkURL = link.find('.te-url-input');
+    this.linkConfirm = link.find('.te-ok-button');
+    this.linkCancel = link.find('.te-close-button');
+
+    const color = Selector('.tui-popup-color');
+    this.colorConfirm = color.find('.te-apply-button');
+  }
+}
+
 class Markdown {
   constructor() {
     const editor = Selector('.te-md-container .te-editor');
@@ -42,9 +65,41 @@ class Markdown {
   }
 }
 
-export default new class Editor {
-  constructor () {
-    this.toolbar = new Toolbar();
-    this.markdown = new Markdown();
+class Wysiwyg {
+  constructor() {
+    const editor = Selector('.te-ww-container .te-editor');
+    const contentArea = editor.find('.tui-editor-contents');
+
+    this.blocks = contentArea.find('div,h1,h2,h3,h4,h5,h6,ul,ol,hr,table,pre');
   }
-}();
+}
+
+class Editor {
+  ZWS = String.fromCharCode(0x200b);
+
+  toolbar = new Toolbar();
+
+  tabbar = new Tabbar();
+
+  markdown = new Markdown();
+
+  wysiwyg = new Wysiwyg();
+
+  popup = new Popup();
+
+  async showWysiwyg() {
+    await t.click(this.tabbar.wysiwyg);
+    await this.fixWysiwygCaret();
+  }
+
+  async fixWysiwygCaret() {
+    await t.click(this.toolbar.code)
+      .click(this.toolbar.code);
+  }
+
+  async showMarkdown() {
+    await t.click(this.tabbar.markdown);
+  }
+}
+
+export default new Editor();
