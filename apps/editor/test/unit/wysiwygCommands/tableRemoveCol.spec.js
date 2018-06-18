5b9e791fa02925b06fe3291882cf2aee252d7618
KyuWoo Choi <kyuwoo.choi@gmail.com>
2018-06-18 14:56:10 +0900

    fix: deleting all table column breaks markdown (close #215) (#216)
    * fix: deleting all table column breaks markdown
    
    * refactor: apply code review (ref #216)

--- apps/editor/test/unit/wysiwygCommands/tableRemoveCol.spec.js
+++ apps/editor/test/unit/wysiwygCommands/tableRemoveCol.spec.js
@@ -88,6 +88,32 @@ describe('Table - RemoveCol', () => {
     expect(wwe.get$Body().find('tbody td').length).toBe(2);
   });
 
+  it('should not remove cols if selected all col', () => {
+    const sq = wwe.getEditor();
+    const range = sq.getSelection().cloneRange();
+
+    sq.setHTML(
+      `<table>
+          <thead>
+              <tr><th>1</th><th>2</th></tr>
+          </thead>
+          <tbody>
+              <tr><td class="te-cell-selected">3</td><td class="te-cell-selected">4</td></tr>
+              <tr><td>5</td><td>6</td></tr>
+          </tbody>
+      </table>`
+    );
+
+    range.setStartAfter(wwe.get$Body().find('tbody td')[0].firstChild);
+    range.setEndAfter(wwe.get$Body().find('tbody td')[1].firstChild);
+    sq.setSelection(range);
+
+    RemoveCol.exec(wwe);
+
+    expect(wwe.get$Body().find('thead th').length).toBe(2);
+    expect(wwe.get$Body().find('tbody td').length).toBe(4);
+  });
+
   it('should remove all columns if there are multiple tds in range', () => {
     const sq = wwe.getEditor();
     const range = sq.getSelection().cloneRange();
