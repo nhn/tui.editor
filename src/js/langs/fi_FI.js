5a8e2b6f9510acdecc544d671a6597a20bd1d33e
Pikseli <tomi.mynttinen@iki.fi>
2018-08-23 14:20:06 +0300

    Create fi_FI.js

---
+++ src/js/langs/fi_FI.js
@@ -0,0 +1,53 @@
+/**
+* @fileoverview I18N for Finnish
+* @author Tomi Mynttinen <pikseli@iki.fi>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['fi', 'fi_FI'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Kirjoita',
+  'Preview': 'Esikatselu',
+  'Headings': 'Otsikot',
+  'Paragraph': 'Kappale',
+  'Bold': 'Lihavointi',
+  'Italic': 'Kursivointi',
+  'Strike': 'Yliviivaus',
+  'Code': 'Koodi',
+  'Line': 'Vaakaviiva',
+  'Blockquote': 'Lainaus',
+  'Unordered list': 'Luettelo',
+  'Ordered list': 'Numeroitu luettelo',
+  'Task': 'Tehtävä',
+  'Indent': 'Suurenna sisennystä',
+  'Outdent': 'Pienennä sisennystä',
+  'Insert link': 'Lisää linkki',
+  'Insert CodeBlock': 'Lisää koodia',
+  'Insert table': 'Lisää taulukko',
+  'Insert image': 'Lisää kuva',
+  'Heading': 'Otsikko',
+  'Image URL': 'Kuvan URL',
+  'Select image file': 'Valitse kuvatiedosto',
+  'Description': 'Kuvaus',
+  'OK': 'OK',
+  'More': 'Lisää',
+  'Cancel': 'Peruuta',
+  'File': 'Tiedosto',
+  'URL': 'URL',
+  'Link text': 'Linkkiteksti',
+  'Add row': 'Lisää rivi',
+  'Add col': 'Lisää sarake',
+  'Remove row': 'Poista rivi',
+  'Remove col': 'Poista sarake',
+  'Align left': 'Tasaus vasemmalle',
+  'Align center': 'Keskitä',
+  'Align right': 'Tasaus oikealle',
+  'Remove table': 'Poista taulukko',
+  'Would you like to paste as table?': 'Haluatko liittää taulukkomuodossa?',
+  'Text color': 'Tekstin väri',
+  'Auto scroll enabled': 'Automaattinen skrollaus käytössä',
+  'Auto scroll disabled': 'Automaattinen skrollaus pois käytöstä',
+  'Cannot paste values other than a table in the cell selection state': 'Vain taulukko voidaan liittää solunvalintatilassa.',
+  'Choose language': 'Valitse kieli'
+});
