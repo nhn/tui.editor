cb0060e0e4daa29e5e0d325f2e90c7f74d485016
Dmitrij Tkačenko <dmitrij.tkacenko@scalesoft.cz>
2018-10-23 12:53:01 +0200

    feat: add Czech language

---
+++ apps/editor/src/js/langs/cs_CZ.js
@@ -0,0 +1,53 @@
+/**
+* @fileoverview I18N for Czech
+* @author Dmitrij Tkačenko <dmitrij.tkacenko@scalesoft.cz>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['cs', 'cs_CZ'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Napsat',
+  'Preview': 'Náhled',
+  'Headings': 'Nadpisy',
+  'Paragraph': 'Odstavec',
+  'Bold': 'Tučné',
+  'Italic': 'Kurzíva',
+  'Strike': 'Přeškrtnuté',
+  'Code': 'Kód',
+  'Line': 'Vodorovná čára',
+  'Blockquote': 'Citace',
+  'Unordered list': 'Seznam s odrážkami',
+  'Ordered list': 'Číslovaný seznam',
+  'Task': 'Úkol',
+  'Indent': 'Zvětšit odsazení',
+  'Outdent': 'Zmenšit odsazení',
+  'Insert link': 'Vložit odkaz',
+  'Insert CodeBlock': 'Vložit blok kódu',
+  'Insert table': 'Vložit tabulku',
+  'Insert image': 'Vložit obrázek',
+  'Heading': 'Nadpis',
+  'Image URL': 'URL obrázku',
+  'Select image file': 'Vybrat obrázek',
+  'Description': 'Popis',
+  'OK': 'OK',
+  'More': 'Více',
+  'Cancel': 'Zrušit',
+  'File': 'Soubor',
+  'URL': 'URL',
+  'Link text': 'Text odkazu',
+  'Add row': 'Přidat řádek',
+  'Add col': 'Přidat sloupec',
+  'Remove row': 'Odebrat řádek',
+  'Remove col': 'Odebrat sloupec',
+  'Align left': 'Zarovnat vlevo',
+  'Align center': 'Zarovnat na střed',
+  'Align right': 'Zarovnat vpravo',
+  'Remove table': 'Odstranit tabulku',
+  'Would you like to paste as table?': 'Chcete vložit jako tabulku?',
+  'Text color': 'Barva textu',
+  'Auto scroll enabled': 'Automatické rolování zapnuto',
+  'Auto scroll disabled': 'Automatické rolování vypnuto',
+  'Cannot paste values ​​other than a table in the cell selection state': 'Nelze vkládat jiné hodnoty než tabulka ve stavu výběru buněk',
+  'Choose language': 'Vybrat jazyk'
+});
