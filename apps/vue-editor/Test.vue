3585a1bf6191b6944109e320bfc2da726b3ffee2
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-11-06 15:46:12 +0900

    test: Add Test.vue for UI testing for development. (#2)
    Prerequisition for testing
    - Vue CLI  3.  just run `vue serve test.vue`

---
+++ apps/vue-editor/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>🍞📝Toast UI Markdown Editor + Vue</h1>
+    <editor />
+  </div>
+</template>
+
+<script>
+import {Editor} from './src/index.js';
+
+export default {
+    components: {
+        Editor
+    }
+};
+</script>
+
+<style>
+@import 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.41.0/codemirror.css';
+@import 'https://uicdn.toast.com/tui-editor/latest/tui-editor-contents.css';
+@import 'https://uicdn.toast.com/tui-editor/latest/tui-editor.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/github.min.css';
+
+.container {
+  width: 960px;
+}
+</style>
