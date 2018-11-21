eeef5bb52141ff8d7fb0d1364d4fb69f3e1a20d5
Marcin Golonka <marcin.golonka@raltech.pl>
2018-11-21 15:28:53 +0100

    Add polish translate

---
+++ src/js/langs/pl_PL.js
@@ -0,0 +1,54 @@
+/**
+* @fileoverview I18N for Polish
+* @author Marcin Golonka <kontakt@mgolonka.pl>
+*/
+import i18n from '../i18n';
+
+i18n.setLanguage(['en', 'en_US'], {
+  'Markdown': 'Markdown',
+  'WYSIWYG': 'WYSIWYG',
+  'Write': 'Napisz',
+  'Preview': 'Podgląd',
+  'Headings': 'Nagłówki',
+  'Paragraph': 'Akapit',
+  'Bold': 'Pogrubienie',
+  'Italic': 'Kursywa',
+  'Strike': 'Strike',
+  'Code': 'Inline code',
+  'Line': 'Linia',
+  'Blockquote': 'Cytat',
+  'Unordered list': 'Lista nieposortowana',
+  'Ordered list': 'Lsita posortowana',
+  'Task': 'Zadanie',
+  'Indent': 'Wcięcie',
+  'Outdent': 'Zminiejsz wcięcie',
+  'Insert link': 'Wstaw odnośnik',
+  'Insert CodeBlock': 'Wstaw kod',
+  'Insert table': 'Wstaw tabelę',
+  'Insert image': 'Wstap obraz',
+  'Heading': 'Nagłówek',
+  'Image URL': 'URL Obrazu',
+  'Select image file': 'Wybierz plik obrazu',
+  'Description': 'Opis',
+  'OK': 'OK',
+  'More': 'Więcej',
+  'Cancel': 'Anuluj',
+  'File': 'Plik',
+  'URL': 'URL',
+  'Link text': 'Opis linku',
+  'Add row': 'Dodaj rząd',
+  'Add col': 'Dodaj kolumnę',
+  'Remove row': 'Usuń rząd',
+  'Remove col': 'Usuń kolumnę',
+  'Align left': 'Wyśrodkuj do lewej',
+  'Align center': 'Wyśrodkuj do środka',
+  'Align right': 'Wyśrodkuj do prawej',
+  'Remove table': 'Usuń tabelę',
+  'Would you like to paste as table?': 'Czy chcesz wkleić tabelę?',
+  'Text color': 'Kolor tekstu',
+  'Auto scroll enabled': 'Auto scroll włączony',
+  'Auto scroll disabled': 'Auto scroll wyłączony',
+  'Cannot paste values ​​other than a table in the cell selection state':
+        'Nie można wkleić innych wartości niż tabela w obecnym stanie zaznaczenia',
+  'Choose language': 'Wybierz język'
+});
