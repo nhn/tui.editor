4b01ea70f2afeeee3700df944efcdc7989629ffd
이소희 <42666492+sohee-lee7@users.noreply.github.com>
2019-02-18 19:53:54 +0900

    fix: consider depth when apply list (close: #392) (#395)
    Previously markdown list don't care depth of list,
    so change implement to check depth when apply list.
    Most code move mdListMnager because OL, UL, Task use same code.
    
    * fix: All list are changed regardless of depth
    
    * fix: find one depth list in same list
    
    * refactor: markdown list manager
    
    * refactor: insert blank line
    
    * refactor: same entry function for change ol, ul, task

--- src/js/markdownCommands/listRegex.js
+++
@@ -1,4 +0,0 @@
-export const FIND_MD_OL_RX = /^[ \t]*[\d]+\. .*/;
-export const FIND_MD_UL_RX = /^[ \t]*[-*] .*/;
-export const FIND_MD_TASK_RX = /^[ \t]*([*-] |[\d]+\. )(\[[ xX]] ).*/;
-export const FIND_MD_UL_TASK_RX = /^[ \t]*[*-] (\[[ xX]] ).*/;
--- src/js/markdownCommands/ul.js
+++ src/js/markdownCommands/ul.js
@@ -3,15 +3,6 @@
  * @author NHN Ent. FE Development Lab <dl_javascript@nhnent.com>
  */
 import CommandManager from '../commandManager';
-import {
-  FIND_MD_OL_RX,
-  FIND_MD_UL_RX,
-  FIND_MD_TASK_RX,
-  FIND_MD_UL_TASK_RX
-} from './listRegex';
-
-const MD_UL_TASK_SYNTAX_RX = /([-*])( \[[ xX]]) /;
-const MD_UL_OR_UL_TASK_SYNTAX_RX = /[\d]+\.( \[[ xX]])? /;
 
 /**
  * UL
@@ -28,59 +19,11 @@ const UL = CommandManager.command('markdown', /** @lends UL */{
    * @param {MarkdownEditor} mde MarkdownEditor instance
    */
   exec(mde) {
-    const cm = mde.getEditor();
-    const doc = cm.getDoc();
     const range = mde.getCurrentRange();
     const listManager = mde.componentManager.getManager('list');
-    const lineRange = listManager.expandLineRangeIfNeed(doc, range, isOlOrTask);
-    const startLineNumber = lineRange.start;
-    const endLineNumber = lineRange.end;
-    let line, currentLineStart;
 
-    for (let i = startLineNumber; i <= endLineNumber; i += 1) {
-      currentLineStart = {
-        line: i,
-        ch: 0
-      };
-
-      line = doc.getLine(i);
-
-      if (listManager.isListOrParagraph(line)) {
-        if (isUlTask(line)) {
-          listManager.replaceLineText(doc, i, MD_UL_TASK_SYNTAX_RX, '$1 ');
-        } else if (isOlOrTask(line)) {
-          listManager.replaceLineText(doc, i, MD_UL_OR_UL_TASK_SYNTAX_RX, '* ');
-        } else if (!line.match(FIND_MD_UL_RX)) {
-          doc.replaceRange('* ', currentLineStart);
-        }
-
-        if (i === endLineNumber) {
-          listManager.appendBlankLineIfNeed(cm, i, endLineNumber, startLineNumber);
-        }
-      } else {
-        break;
-      }
-    }
-    cm.focus();
+    listManager.changeSyntax(range, 'ul');
   }
 });
-
-/**
- * Return whether the given line is UL TASK
- * @param {string} line Line text
- * @returns {boolean}
- */
-function isUlTask(line) {
-  return !!(line && line.match(FIND_MD_UL_TASK_RX));
-}
-
-/**
- * Return whether passed line is OL or TASK or neither
- * @param {string} line Line text
- * @returns {boolean}
- */
-function isOlOrTask(line) {
-  return !!(line && (line.match(FIND_MD_TASK_RX) || line.match(FIND_MD_OL_RX)));
-}
 
 export default UL;
