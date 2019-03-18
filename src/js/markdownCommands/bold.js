753c0848fa3fee843e993a80ca05cd9dc28e017a
이소희 <42666492+sohee-lee7@users.noreply.github.com>
2019-03-18 15:54:42 +0900

    feat: toggle bold, italic, strike (close: #434) (#443)
    * feat: toggle bold, italic, strike (close: #434)
    
    * refactor: remove meaningless comment
    
    * refactor: reflect code review
    
    * refactor: reflect code review
    
    * fix: rename file
    
    * doc: more detail comment

--- src/js/markdownCommands/bold.js
+++ src/js/markdownCommands/bold.js
@@ -3,9 +3,11 @@
 * @author NHN Ent. FE Development Lab <dl_javascript@nhnent.com>
 */
 import CommandManager from '../commandManager';
+import {changeSyntax} from './emphasisCommon';
 
-const boldRangeRegex = /^[*_]{2,}[^*_]+[*_]{2,}$/;
-const boldContentRegex = /[*_]{2,}([^*_]+)[*_]{2,}/g;
+const boldRangeRegex = /^(\*{2}|_{2}).*\1$/;
+const boldContentRegex = /[*_]{2,}([^*_]*)[*_]{2,}/g;
+const boldSymbol = '**';
 
 /**
  * Bold
@@ -24,109 +26,11 @@ const Bold = CommandManager.command('markdown', /** @lends Bold */{
   exec(mde) {
     const cm = mde.getEditor();
     const doc = cm.getDoc();
+    const originRange = mde.getRange();
 
-    const cursor = doc.getCursor();
-    let selection = doc.getSelection();
-    const isEmpty = !selection;
-
-    // if selection is empty, expend selection to detect a syntax
-    if (isEmpty && cursor.ch > 1) {
-      const tmpSelection = this.expendSelection(doc, cursor);
-      selection = tmpSelection || selection;
-    }
-
-    const isRemoved = this.isNeedRemove(selection);
-    let result;
-    if (isRemoved) {
-      result = this.remove(selection);
-      result = this._removeBoldSyntax(result);
-    } else {
-      result = this._removeBoldSyntax(selection);
-      result = this.append(result);
-    }
-
-    doc.replaceSelection(result, 'around');
-
-    if (isEmpty && !isRemoved) {
-      this.setCursorToCenter(doc, cursor);
-    }
+    changeSyntax(doc, originRange, boldSymbol, boldRangeRegex, boldContentRegex);
 
     cm.focus();
-  },
-
-  /**
-   * test it has bold
-   * @param {string} text - text selected
-   * @returns {boolean} - true if it has bold
-   */
-  isNeedRemove(text) {
-    return boldRangeRegex.test(text);
-  },
-
-  /**
-   * apply bold
-   * @param {string} text - text selected
-   * @returns {string} - bold text
-   */
-  append(text) {
-    return `**${text}**`;
-  },
-
-  /**
-   * remove bold
-   * @param {string} text - text selected
-   * @returns {string} - un-bold text
-   */
-  remove(text) {
-    return text.substr(2, text.length - 4);
-  },
-
-  /**
-   * expand selection
-   * @param {CodeMirror.doc} doc - codemirror document
-   * @param {object} cursor - codemirror cursor
-   * @returns {string} - text selected
-   */
-  expendSelection(doc, cursor) {
-    const tmpSelection = doc.getSelection();
-    let result;
-    const start = {
-      line: cursor.line,
-      ch: cursor.ch - 2
-    };
-    const end = {
-      line: cursor.line,
-      ch: cursor.ch + 2
-    };
-
-    doc.setSelection(start, end);
-
-    if (tmpSelection === '****' || tmpSelection === '____') {
-      result = tmpSelection;
-    } else {
-      doc.setSelection(cursor);
-    }
-
-    return result;
-  },
-
-  /**
-   * move cursor to center
-   * @param {CodeMirror.doc} doc - codemirror document
-   * @param {object} cursor - codemirror cursor
-   */
-  setCursorToCenter(doc, cursor) {
-    doc.setCursor(cursor.line, cursor.ch + 2);
-  },
-
-  /**
-   * remove bold syntax in the middle of given text
-   * @param {string} text - text selected
-   * @returns {string} - text eliminated all bold in the middle of it's content
-   * @private
-   */
-  _removeBoldSyntax(text) {
-    return text ? text.replace(boldContentRegex, '$1') : '';
   }
 });
 
--- test/unit/markdownCommands/italic.spec.js
+++ test/unit/markdownCommands/italic.spec.js
@@ -61,6 +61,62 @@ describe('Italic', () => {
       expect(cm.getValue()).toEqual(['*mytext1*', '', 'mytext2', 'mytext3'].join('\n'));
     });
 
+    it('in empty and collapsed bold', () => {
+      cm.setValue('****');
+      doc.setCursor(0, 2);
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('******');
+    });
+
+    it('selected only text collapsed bold', () => {
+      cm.setValue('**test**');
+      doc.setSelection({
+        line: 0,
+        ch: 2
+      }, {
+        line: 0,
+        ch: 6
+      });
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('***test***');
+    });
+
+    it('selected all text collapsed bold', () => {
+      cm.setValue('**test**');
+      doc.setSelection({
+        line: 0,
+        ch: 0
+      }, {
+        line: 0,
+        ch: 8
+      });
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('***test***');
+    });
+
+    it('selected text have bold text and collaped strike', () => {
+      cm.setValue('~~**test**~~');
+      doc.setSelection({
+        line: 0,
+        ch: 0
+      }, {
+        line: 0,
+        ch: 12
+      });
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('*~~**test**~~*');
+    });
+  });
+
+  describe('remove italic', () => {
     it('should remove italic syntax in the middle of the given range', () => {
       cm.setValue('my _text_ 1');
 
@@ -75,6 +131,80 @@ describe('Italic', () => {
       Italic.exec(mde);
 
       expect(cm.getValue()).toEqual('*my text 1*');
+    });
+
+    it('in the empty string collapsed italic', () => {
+      cm.setValue('**');
+      doc.setCursor(0, 1);
+
+      Italic.exec(mde);
+      expect(cm.getValue()).toEqual('');
+    });
+
+    it('in the empty string collapsed italic and blod', () => {
+      cm.setValue('******');
+      doc.setCursor(0, 3);
+
+      Italic.exec(mde);
+      expect(cm.getValue()).toEqual('****');
+    });
+
+    it('selected only text that is collapsed italic', () => {
+      cm.setValue('*test*');
+      doc.setSelection({
+        line: 0,
+        ch: 1
+      }, {
+        line: 0,
+        ch: 5
+      });
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('test');
+    });
+
+    it('selected all text that is collapsed italic', () => {
+      cm.setValue('*test*');
+      doc.setSelection({
+        line: 0,
+        ch: 0
+      }, {
+        line: 0,
+        ch: 6
+      });
+
+      Italic.exec(mde);
+
+      expect(cm.getValue()).toEqual('test');
+    });
+
+    it('selected only text collapsed italic and bold', () => {
+      cm.setValue('***test***');
+      doc.setSelection({
+        line: 0,
+        ch: 3
+      }, {
+        line: 0,
+        ch: 7
+      });
+
+      Italic.exec(mde);
+      expect(cm.getValue()).toEqual('**test**');
+    });
+
+    it('selected all text collapsed italic and bold', () => {
+      cm.setValue('***test***');
+      doc.setSelection({
+        line: 0,
+        ch: 0
+      }, {
+        line: 0,
+        ch: 10
+      });
+
+      Italic.exec(mde);
+      expect(cm.getValue()).toEqual('**test**');
     });
   });
 });
