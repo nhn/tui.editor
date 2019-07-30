df7f3d34b4c996f46bdb92d37080bf1ea7b49b4f
이소희 <42666492+sohee-lee7@users.noreply.github.com>
2019-07-30 19:26:59 +0900

    fix: space in span disappear when covert to wysiwyg (#586)
    * Revert "fix: space is not convert html entity when pasting (fix: #432)"
    
    This reverts commit a75fd61ffe645eabef9f4edabb7e9a148adb9aec.
    
    * fix: space disappear when convert to markdown (fix: #432, #559)
    
    * refactor: reflect code review

--- apps/editor/src/js/wysiwygEditor.js
+++ apps/editor/src/js/wysiwygEditor.js
@@ -23,9 +23,11 @@ import CodeBlockGadget from './ui/codeBlockGadget';
 
 const keyMapper = KeyMapper.getSharedInstance();
 
-const FIND_EMPTY_LINE = /<([a-z]+|h\d)>(<br>|<br \/>)<\/\1>/gi,
-  FIND_UNNECESSARY_BR = /(?:<br>|<br \/>)<\/(.+?)>/gi,
-  FIND_BLOCK_TAGNAME_RX = /\b(H[\d]|LI|P|BLOCKQUOTE|TD|PRE)\b/;
+const FIND_EMPTY_LINE = /<([a-z]+|h\d)>(<br>|<br \/>)<\/\1>/gi;
+const FIND_UNNECESSARY_BR = /(?:<br>|<br \/>)<\/(.+?)>/gi;
+const FIND_BLOCK_TAGNAME_RX = /\b(H[\d]|LI|P|BLOCKQUOTE|TD|PRE)\b/;
+const FIND_OPENING_SPAN_WITH_SPACE = /<span([^>]*)>[\u0020]/g;
+const FIND_CLOSING_SPAN_WITH_SPACE = /[\u0020]<\/span>/g;
 const FIND_TABLE_AND_HEADING_RX = /^(TABLE|H[1-6])$/;
 
 const EDITOR_CONTENT_CSS_CLASSNAME = 'tui-editor-contents';
@@ -894,6 +896,10 @@ class WysiwygEditor {
 
       return result;
     });
+
+    // replace a space of the first and end in sapn tag to &nbsp;.
+    html = html.replace(FIND_OPENING_SPAN_WITH_SPACE, '<span$1>&nbsp;');
+    html = html.replace(FIND_CLOSING_SPAN_WITH_SPACE, '&nbsp;</span>');
 
     // remove unnecessary brs
     html = html.replace(FIND_UNNECESSARY_BR, '</$1>');
