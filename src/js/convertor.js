107b7c779ea5440127611e6955d78eeb9da56620
seonim-ryu <seonim.ryu@nhn.com>
2019-08-30 17:33:53 +0900

    fix: update regular expression

--- src/js/convertor.js
+++ src/js/convertor.js
@@ -165,7 +165,7 @@ class Convertor {
 
     $wrapperDiv.find('code, pre').each((i, codeOrPre) => {
       const $code = $(codeOrPre);
-      $code.html($code.html().replace(/(\sdata-tomark-pass\s)(\/?)&gt;/g, '$2&gt;'));
+      $code.html($code.html().replace(/\sdata-tomark-pass\s(\/?)&gt;/g, '$1&gt;'));
     });
 
     renderedHTML = $wrapperDiv.html();
