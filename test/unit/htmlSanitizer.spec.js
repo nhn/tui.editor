70716fd0e6eec44ae99e8bc5d4c74fd20e3fbb22
TaeGeun Moon <ahn_v3@naver.com>
2019-09-13 02:05:12 +0900

    test: add example for htmlSanitizer (#628)
    test: add example for htmlSanitizer (#628)

--- test/unit/htmlSanitizer.spec.js
+++ test/unit/htmlSanitizer.spec.js
@@ -41,5 +41,10 @@ describe('htmlSanitizer', function() {
       expect($circle.attr('stroke-width')).toBeTruthy();
       expect($circle.attr('fill')).toBeTruthy();
     });
+
+    it('Remove attributes with invalid value', function() {
+      expect(htmlSanitizer('<a href="javascript:alert();">xss</a>', true)).toEqual('<a>xss</a>');
+      expect(htmlSanitizer('<a href="#">benign</a>', true)).toEqual('<a href="#">benign</a>');
+    });
   });
 });
