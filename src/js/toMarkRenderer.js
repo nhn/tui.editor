75370365980faa8f58f1a6382ae74f1cc0d8873b
DongWoo Kim <dongwoo.kim@nhnent.com>
2019-09-23 11:54:22 +0900

    refactor: rename isInvalidEmphasisToken to isValidDelimiterRun

--- src/js/toMarkRenderer.js
+++ src/js/toMarkRenderer.js
@@ -8,21 +8,31 @@ function isTextNode(node) {
   return node && node.nodeType === Node.TEXT_NODE;
 }
 
-function isInvalidEmphasisToken(node) {
+/**
+ * Check if given node is valid delimiter run.
+ * According to common-mark spec, following examples are not valid delimiter runs.
+ * 1. opening (*|**) preceded by an alphanumeric and followed by a punctuation.
+ *    (ex: a**~~c~~b**)
+ * 2. closing (*|**) preceded by a punctuation and followed by an alphanumeric.
+ *    (ex: **b~~c~~**a)
+ * @See {@link https://spec.commonmark.org/0.29/#delimiter-run}
+ * @See {@link https://github.com/commonmark/commonmark-spec/issues/611#issuecomment-533578503}
+ **/
+function isValidDelimiterRun(node) {
   const isInvalidOpener = isTextNode(node.previousSibling) && isElementNode(node.firstChild);
   const isInvalidCloser = isTextNode(node.nextSibling) && isElementNode(node.lastChild);
 
-  return isInvalidOpener || isInvalidCloser;
+  return !isInvalidOpener && !isInvalidCloser;
 }
 
 function convertEmphasis(node, subContent, token) {
-  if (isInvalidEmphasisToken(node)) {
-    const tagName = node.nodeName.toLowerCase();
-
-    return `<${tagName}>${subContent}</${tagName}>`;
+  if (isValidDelimiterRun(node)) {
+    return `${token}${subContent}${token}`;
   }
 
-  return `${token}${subContent}${token}`;
+  const tagName = node.nodeName.toLowerCase();
+
+  return `<${tagName}>${subContent}</${tagName}>`;
 }
 
 export default toMark.Renderer.factory(toMark.gfmRenderer, {
