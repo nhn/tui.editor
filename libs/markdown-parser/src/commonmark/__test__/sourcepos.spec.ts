f1182d34b8ccee78efd0220a582fe2159a661cbe
DongWoo Kim <dongwoo.kim@nhnent.com>
2020-01-09 17:16:31 +0900

    feat: add sourcepos to inline nodes

--- libs/markdown-parser/src/commonmark/__test__/sourcepos.spec.ts
+++ libs/markdown-parser/src/commonmark/__test__/sourcepos.spec.ts
@@ -1,4 +1,5 @@
 import { Parser } from '../blocks';
+import { HtmlRenderer } from '../render/html';
 
 const reader = new Parser();
 
@@ -73,6 +74,117 @@ describe('paragraph', () => {
       [1, 13]
     ]);
   });
+
+  it('text and image', () => {
+    const root = reader.parse('Hello ![World](http://nhn.com)');
+    const text = root.firstChild!.firstChild!;
+    const image = text.next!;
+    const imageText = image.firstChild!;
+
+    expect(text.sourcepos).toEqual([
+      [1, 1],
+      [1, 6]
+    ]);
+    expect(image.sourcepos).toEqual([
+      [1, 7],
+      [1, 30]
+    ]);
+    expect(imageText.sourcepos).toEqual([
+      [1, 9],
+      [1, 13]
+    ]);
+  });
+
+  it('text and link', () => {
+    const root = reader.parse('Hello [World](http://nhn.com)');
+    const text = root.firstChild!.firstChild!;
+    const link = text.next!;
+    const linkText = link.firstChild!;
+
+    expect(text.sourcepos).toEqual([
+      [1, 1],
+      [1, 6]
+    ]);
+    expect(link.sourcepos).toEqual([
+      [1, 7],
+      [1, 29]
+    ]);
+    expect(linkText.sourcepos).toEqual([
+      [1, 8],
+      [1, 12]
+    ]);
+  });
+
+  it('text and codespan', () => {
+    const root = reader.parse('Hello ``World``');
+    const text = root.firstChild!.firstChild!;
+    const code = text.next!;
+
+    expect(text.sourcepos).toEqual([
+      [1, 1],
+      [1, 6]
+    ]);
+    expect(code.tickCount).toBe(2);
+    expect(code.sourcepos).toEqual([
+      [1, 7],
+      [1, 15]
+    ]);
+  });
+
+  it('text and raw html', () => {
+    const root = reader.parse('Hello <strong>World</strong>');
+    const text1 = root.firstChild!.firstChild!;
+    const html1 = text1.next!;
+    const text2 = html1.next!;
+    const html2 = text2.next!;
+
+    expect(text1.sourcepos).toEqual([
+      [1, 1],
+      [1, 6]
+    ]);
+    expect(html1.sourcepos).toEqual([
+      [1, 7],
+      [1, 14]
+    ]);
+    expect(text2.sourcepos).toEqual([
+      [1, 15],
+      [1, 19]
+    ]);
+    expect(html2.sourcepos).toEqual([
+      [1, 20],
+      [1, 28]
+    ]);
+  });
+
+  it('autolink', () => {
+    const root = reader.parse('Hello <http://nhn.com>');
+    const link = root.firstChild!.firstChild!.next!;
+    const linkText = link.firstChild!;
+
+    expect(link.sourcepos).toEqual([
+      [1, 7],
+      [1, 22]
+    ]);
+    expect(linkText.sourcepos).toEqual([
+      [1, 8],
+      [1, 21]
+    ]);
+  });
+
+  it('autolink (mailto)', () => {
+    const root = reader.parse('Hello <world@nhn.com>');
+    const link = root.firstChild!.firstChild!.next!;
+    const linkText = link.firstChild!;
+
+    expect(link.sourcepos).toEqual([
+      [1, 7],
+      [1, 21]
+    ]);
+    expect(linkText.sourcepos).toEqual([
+      [1, 8],
+      [1, 20]
+    ]);
+  });
 });
 
 describe('softbreak and linebreak', () => {
@@ -141,7 +253,7 @@ describe('softbreak and linebreak', () => {
   });
 });
 
-describe('header', () => {
+describe('atx header', () => {
   it('text and emphasis', () => {
     const root = reader.parse('# Hello *World*');
     const text = root.firstChild!.firstChild!;
@@ -162,7 +274,7 @@ describe('header', () => {
     ]);
   });
 
-  it.only('text and emphasis (header level 3)', () => {
+  it('text and emphasis (header level 3)', () => {
     const root = reader.parse('### Hello *World*');
     const text = root.firstChild!.firstChild!;
     const emph = text.next!;
@@ -183,44 +295,67 @@ describe('header', () => {
   });
 });
 
-describe('link and image', () => {
-  it.only('image', () => {
-    const root = reader.parse('Hello ![World](http://nhn.com)');
-    const text = root.firstChild!.firstChild!;
-    const image = text.next!;
-    const imageText = image.firstChild!;
+describe('list items', () => {
+  it('with columns', () => {
+    const root = reader.parse('   - Hello\n\n     World');
+    const listItem = root.firstChild!.firstChild!;
+    const para1 = listItem.firstChild!;
+    const para1Text = para1.firstChild!;
+    const para2 = para1.next!;
+    const para2Text = para2.firstChild!;
 
-    expect(text.sourcepos).toEqual([
-      [1, 1],
-      [1, 6]
+    expect(para1Text.sourcepos).toEqual([
+      [1, 6],
+      [1, 10]
     ]);
-    expect(image.sourcepos).toEqual([
-      [1, 7],
-      [1, 30]
-    ]);
-    expect(imageText.sourcepos).toEqual([
-      [1, 9],
-      [1, 13]
+    expect(para2Text.sourcepos).toEqual([
+      [3, 6],
+      [3, 10]
     ]);
   });
+});
 
-  it('link', () => {
-    const root = reader.parse('Hello [World](http://nhn.com)');
-    const text = root.firstChild!.firstChild!;
-    const link = text.next!;
-    const linkText = link.firstChild!;
+describe('block quote', () => {
+  it('nested paragraph', () => {
+    const root = reader.parse('> Hello\n> > World');
+    const quote1 = root.firstChild!;
+    const para1 = quote1.firstChild!;
+    const quote2 = para1.next!;
+    const para2 = quote2.firstChild!;
 
-    expect(text.sourcepos).toEqual([
+    expect(para1.firstChild!.sourcepos).toEqual([
+      [1, 3],
+      [1, 7]
+    ]);
+    expect(para2.firstChild!.sourcepos).toEqual([
+      [2, 5],
+      [2, 9]
+    ]);
+  });
+});
+
+describe('block quote', () => {
+  it.skip('merge text nodes', () => {
+    const root = reader.parse(['\\ Text *', '[ Text !', '![ Text ]'].join('\n'));
+    const text1 = root.firstChild!.firstChild!;
+    const text2 = text1.next!.next!;
+    const text3 = text2.next!.next!;
+
+    expect(text1.literal).toBe('\\ Text *');
+    expect(text1.sourcepos).toEqual([
       [1, 1],
-      [1, 6]
+      [1, 8]
     ]);
-    expect(link.sourcepos).toEqual([
-      [1, 7],
-      [1, 29]
+    expect(text2.literal).toBe('[ Text !');
+    expect(text2.sourcepos).toEqual([
+      [2, 1],
+      [2, 8]
     ]);
-    expect(linkText.sourcepos).toEqual([
-      [1, 8],
-      [1, 12]
+
+    expect(text3.literal).toBe('![ Text ]');
+    expect(text3.sourcepos).toEqual([
+      [3, 1],
+      [3, 9]
     ]);
   });
 });
