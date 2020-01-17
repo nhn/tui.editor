97c0e7bc620af64d130066b82609a494c5d8f1a6
seonim-ryu <seonim.ryu@nhn.com>
2020-01-17 18:11:10 +0900

    feat: apply code review

--- apps/editor/.babelrc.js
+++ apps/editor/.babelrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { spec: true }],
-    ['@babel/plugin-transform-destructuring', { loose: true }]
-  ]
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        loose: true
+      }
+    ]
+  ],
+  plugins: [['@babel/plugin-proposal-class-properties', { spec: true }]]
 };
