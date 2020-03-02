18fd09ece07898b592de86c5d42db0a1b623db71
js87zz <js87zz.lee@nhn.com>
2020-03-02 11:25:39 +0900

    chore: add includes util function

--- apps/editor/src/js/util.js
+++ apps/editor/src/js/util.js
@@ -5,3 +5,12 @@ export const isMac = /Mac/.test(navigator.platform);
 export function sendHostName() {
   sendHostname('editor', 'UA-129966929-1');
 }
+
+export function includes(arr, targetItem) {
+  for (const item of arr) {
+    if (item === targetItem) {
+      return true;
+    }
+  }
+  return false;
+}
