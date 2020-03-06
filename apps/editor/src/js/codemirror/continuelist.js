956189900a037f3586a2621498453a357ac0aac5
jaesung-lee <js87zz.lee@nhn.com>
2020-03-06 20:46:59 +0900

    feat: markdown auto indent(list, blockquote) (#779)
    * feat: remove unnecessary mode for auto indenting
    
    * chore: remove unnecessary comment
    
    * chore: remove unnecessary variable

--- apps/editor/src/js/codemirror/continuelist.js
+++ apps/editor/src/js/codemirror/continuelist.js
@@ -33,24 +33,10 @@ CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
     replacements = [];
   for (var i = 0; i < ranges.length; i++) {
     var pos = ranges[i].head;
-
-    // If we're not in Markdown mode, fall back to normal newlineAndIndent
-    var eolState = cm.getStateAfter(pos.line);
-    var inner = CodeMirror.innerMode(cm.getMode(), eolState);
-    if (inner.mode.name !== 'markdown') {
-      cm.execCommand('newlineAndIndent');
-      return;
-    } else {
-      eolState = inner.state;
-    }
-
-    var inList = eolState.list !== false;
-    var inQuote = eolState.quote !== 0;
-
     var line = cm.getLine(pos.line),
       match = listRE.exec(line);
     var cursorBeforeBullet = /^\s*$/.test(line.slice(0, pos.ch));
-    if (!ranges[i].empty() || (!inList && !inQuote) || !match || cursorBeforeBullet) {
+    if (!ranges[i].empty() || !match || cursorBeforeBullet) {
       cm.execCommand('newlineAndIndent');
       return;
     }
