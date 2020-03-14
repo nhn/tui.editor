c4d81d9e97c10b332c232589f976880c0740fa00
js87zz <js87zz.lee@nhn.com>
2020-03-15 01:25:11 +0900

    chore: fix the duplicate module name

--- apps/vue-editor/index.d.ts
+++ apps/vue-editor/index.d.ts
@@ -1,18 +1,18 @@
 import Vue from 'vue';
-import Editor from '@toast-ui/editor';
-import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
+import ToastuiEditor from '@toast-ui/editor';
+import ToastuiEditorViewer from '@toast-ui/editor/dist/toastui-editor-viewer';
 
 type FunctionKeys<T extends object> = {[K in keyof T]: T[K] extends Function ? K : never}[keyof T];
 
-type EditorFnKeys = FunctionKeys<Editor>;
-type ViewerFnKeys = FunctionKeys<Viewer>;
+type EditorFnKeys = FunctionKeys<ToastuiEditor>;
+type ViewerFnKeys = FunctionKeys<ToastuiEditorViewer>;
 
-export declare class Editor extends Vue {
-  invoke<T extends EditorFnKeys>(fname: T, ...args: Parameters<Editor[T]>): ReturnType<Editor[T]>;
+export class Editor extends Vue {
+  invoke<T extends EditorFnKeys>(fname: T, ...args: Parameters<ToastuiEditor[T]>): ReturnType<ToastuiEditor[T]>;
   getRootElement(): HTMLElement;
 }
 
-export declare class Viewer extends Vue {
-  invoke<T extends ViewerFnKeys>(fname: T, ...args: Parameters<Viewer[T]>): ReturnType<Viewer[T]>;
+export class Viewer extends Vue {
+  invoke<T extends ViewerFnKeys>(fname: T, ...args: Parameters<ToastuiEditorViewer[T]>): ReturnType<ToastuiEditorViewer[T]>;
   getRootElement(): HTMLElement;
 }
