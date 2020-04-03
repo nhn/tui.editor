e6e0a8aeed8ac00cae5275caa1c1a7e807999444
js87zz <js87zz.lee@nhn.com>
2020-04-03 19:24:11 +0900

    feat: add iterateObject, omit helper function

--- libs/toastmark/src/helper.ts
+++ libs/toastmark/src/helper.ts
@@ -5,6 +5,8 @@ export function last<T>(arr: T[] | string) {
   return arr[arr.length - 1];
 }
 
+type OmitedKey<T, K extends keyof T> = keyof Omit<T, K>;
+
 // normalize a reference in reference link (remove []s, trim,
 // collapse internal space, unicode case fold.
 // See commonmark/commonmark.js#168.
@@ -15,4 +17,20 @@ export function normalizeReference(str: string) {
     .replace(/[ \t\r\n]+/, ' ')
     .toLowerCase()
     .toUpperCase();
+}
+
+export function iterateObject<T>(obj: T, iteratee: (key: any, obj: T) => void) {
+  Object.keys(obj).forEach(key => {
+    iteratee(key, obj);
+  });
+}
+
+export function omit<T extends object, K extends keyof T>(obj: T, ...propNames: K[]) {
+  const resultMap = {} as Omit<T, K>;
+  Object.keys(obj).forEach(key => {
+    if (propNames.indexOf(key as K) === -1) {
+      resultMap[key as OmitedKey<T, K>] = obj[key as OmitedKey<T, K>];
+    }
+  });
+  return resultMap;
 }
