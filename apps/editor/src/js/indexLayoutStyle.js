6101ecc6cce87fb35b4ebf059fc085fabab707ef
seonim-ryu <seonim.ryu@nhn.com>
2020-04-22 16:44:40 +0900

    feat: change logic to build layout.css

---
+++ apps/editor/src/js/indexLayoutStyle.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview entry point to extract old.css files
+ * @author NHN FE Development Lab <dl_javascript@nhn.com>
+ */
+
+import '../css/editor.css';
+import '../css/md-syntax-highlighting.css';
