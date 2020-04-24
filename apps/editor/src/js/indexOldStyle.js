f4bcf71136906a118fd2d663685dfb10e4dfc77e
류선임 <seonim.ryu@nhn.com>
2020-04-24 16:31:19 +0900

    feat: change style files (#934)
    * feat: change markdown syntax highlighting style
    
    * feat: change preview style
    
    * feat: add old style of editor
    
    * feat: apply design review

--- apps/editor/src/js/indexOldStyle.js
+++ apps/editor/src/js/indexOldStyle.js
@@ -3,7 +3,7 @@
  * @author NHN FE Development Lab <dl_javascript@nhn.com>
  */
 
-import '../css/editor.css';
-import '../css/contents.css';
+import '../css/old/editor.css';
+import '../css/old/contents.css';
 import '../css/preview-highlighting.css';
 import '../css/old/md-syntax-highlighting.css';
