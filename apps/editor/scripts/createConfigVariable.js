7f44dbd145b148a6ce075efcd9ccd32af2f93b2f
류선임 <seonim.ryu@nhn.com>
2020-05-21 20:03:06 +0900

    feat: add config files to log error of example pages (#963)

---
+++ apps/editor/scripts/createConfigVariable.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const config = require(path.resolve(process.cwd(), 'tuidoc.config.json'));
+const examples = config.examples || {};
+const { filePath, globalErrorLogVariable } = examples;
+
+/**
+ * Get Examples Url
+ */
+function getTestUrls() {
+  if (!filePath) {
+    throw Error('not exist examples path at tuidoc.config.json');
+  }
+
+  const urlPrefix = 'http://nhn.github.io/tui.editor/latest';
+
+  const testUrls = fs.readdirSync(filePath).reduce((urls, fileName) => {
+    if (/html$/.test(fileName)) {
+      urls.push(`${urlPrefix}/${filePath}/${fileName}`);
+    }
+    return urls;
+  }, []);
+
+  fs.writeFileSync('url.txt', testUrls.join(', '));
+}
+
+function getGlobalVariable() {
+  if (!globalErrorLogVariable) {
+    throw Error('not exist examples path at tuidoc.config.json');
+  }
+
+  fs.writeFileSync('errorVariable.txt', globalErrorLogVariable);
+}
+
+getTestUrls();
+getGlobalVariable();
