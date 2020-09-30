6cd3452923dfad5177baf1866e0a54c8ef64ec3a
js87zz <js87zz.lee@nhn.com>
2020-10-01 01:11:09 +0900

    chore: list command test

--- apps/editor/src/__test__/mdCommand.spec.ts
+++ apps/editor/src/__test__/mdCommand.spec.ts
@@ -3,12 +3,22 @@ import { ToastMark } from '@toast-ui/toastmark';
 import MarkdownEditor from '@/markdown/mdEditor';
 import EventEmitter from '@/event/eventEmitter';
 import CommandManager from '@/commands/commandManager';
+import { nbspToSpace } from '@/markdown/helper/pos';
 
 function getTextContent(editor: MarkdownEditor) {
   const { doc } = editor.view.state;
+  const docSize = doc.content.size;
+  let text = '';
 
-  // @ts-ignore
-  return doc.content.textBetween(0, doc.content.size, '\n');
+  doc.nodesBetween(0, doc.content.size, (node, pos) => {
+    if (node.isText) {
+      text += node.text!.slice(Math.max(0, pos) - pos, docSize - pos);
+    } else if (node.isBlock && pos > 0) {
+      text += '\n';
+    }
+  });
+
+  return nbspToSpace(text);
 }
 
 let mde: MarkdownEditor, em: EventEmitter, cmd: CommandManager, container: HTMLElement;
@@ -119,7 +129,7 @@ describe('blockQuote command', () => {
   it('should add blockQuote syntax on empty node', () => {
     cmd.exec('markdown', 'blockQuote');
 
-    expect(getTextContent(mde)).toEqual('>\u00a0');
+    expect(getTextContent(mde)).toEqual('> ');
   });
 
   it('should remove blockQuote syntax', () => {
@@ -163,13 +173,13 @@ describe('hr command', () => {
   it('should add thematicBreak(hr) syntax', () => {
     cmd.exec('markdown', 'hr');
 
-    expect(getTextContent(mde)).toEqual('***\n');
+    expect(getTextContent(mde)).toEqual('\n***\n');
   });
 
   it('should split the paragraph when adding thematicBreak(hr) syntax', () => {
     mde.setMarkdown('paragraph');
 
-    mde.setSelection([0, 2], [0, 4]);
+    mde.setSelection([1, 2], [1, 4]);
     cmd.exec('markdown', 'hr');
 
     expect(getTextContent(mde)).toEqual('pa\n***\ngraph');
@@ -239,7 +249,7 @@ describe('heading command', () => {
   it('should add heading syntax on empty node', () => {
     cmd.exec('markdown', 'heading', { level: 1 });
 
-    expect(getTextContent(mde)).toEqual('#\u00a0');
+    expect(getTextContent(mde)).toEqual('# ');
   });
 
   it('should maintain the heading syntax on same heading level', () => {
@@ -267,5 +277,147 @@ describe('heading command', () => {
     cmd.exec('markdown', 'heading', { level: 2 });
 
     expect(getTextContent(mde)).toEqual('## heading1\n## heading2');
+  });
+});
+
+describe('codeBlock command', () => {
+  it('should add code block syntax', () => {
+    cmd.exec('markdown', 'codeBlock');
+
+    expect(getTextContent(mde)).toEqual('```\n\n```');
+  });
+
+  it('should wrap the selection with code block syntax', () => {
+    mde.setMarkdown(`console.log('codeBlock');`);
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'codeBlock');
+
+    expect(getTextContent(mde)).toEqual("```\nconsole.log('codeBlock');\n```");
+  });
+});
+
+describe('ul command', () => {
+  it('should add bullet list syntax', () => {
+    cmd.exec('markdown', 'ul');
+
+    expect(getTextContent(mde)).toEqual('* ');
+  });
+
+  it('should add bullet list syntax on multi line', () => {
+    mde.setMarkdown('bullet1\nbullet2');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'ul');
+
+    expect(getTextContent(mde)).toEqual('* bullet1\n* bullet2');
+  });
+
+  it('should change ordered list to bullet list', () => {
+    mde.setMarkdown('1. ordered1\n2. ordered2\n3. ordered3');
+
+    mde.setSelection([2, 1], [2, 1]);
+    cmd.exec('markdown', 'ul');
+
+    expect(getTextContent(mde)).toEqual('* ordered1\n* ordered2\n* ordered3');
+  });
+
+  it('should change ordered list to bullet list with depth', () => {
+    mde.setMarkdown('1. ordered1\n2. ordered2\n3. ordered3\n   1. sub1\n   1. sub2');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'ul');
+
+    expect(getTextContent(mde)).toEqual('* ordered1\n* ordered2\n* ordered3\n   * sub1\n   * sub2');
+  });
+});
+
+describe('ol command', () => {
+  it('should add ordered list syntax', () => {
+    cmd.exec('markdown', 'ol');
+
+    expect(getTextContent(mde)).toEqual('1. ');
+  });
+
+  it('should add ordered list syntax on multi line', () => {
+    mde.setMarkdown('ordered1\nordered2');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'ol');
+
+    expect(getTextContent(mde)).toEqual('1. ordered1\n2. ordered2');
+  });
+
+  it('should change bullet list to ordered list', () => {
+    mde.setMarkdown('* bullet1\n* bullet2\n* bullet3');
+
+    mde.setSelection([2, 1], [2, 1]);
+    cmd.exec('markdown', 'ol');
+
+    expect(getTextContent(mde)).toEqual('1. bullet1\n2. bullet2\n3. bullet3');
+  });
+
+  it('should change bullet list to ordered list with depth', () => {
+    mde.setMarkdown('* bullet1\n* bullet2\n* bullet3\n   * sub1\n   * sub2');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'ol');
+
+    expect(getTextContent(mde)).toEqual(
+      '1. bullet1\n2. bullet2\n3. bullet3\n   1. sub1\n   2. sub2'
+    );
+  });
+});
+
+describe('task command', () => {
+  it('should add task list syntax', () => {
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('* [ ] ');
+  });
+
+  it('should add task list syntax on multi line', () => {
+    mde.setMarkdown('task1\ntask2');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('* [ ] task1\n* [ ] task2');
+  });
+
+  it('should add task syntax to ordered list', () => {
+    mde.setMarkdown('1. ordered1\n2. ordered2\n3. ordered3');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('1. [ ] ordered1\n2. [ ] ordered2\n3. [ ] ordered3');
+  });
+
+  it('should add task syntax to bullet list', () => {
+    mde.setMarkdown('* bullet1\n* bullet2\n* bullet3');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('* [ ] bullet1\n* [ ] bullet2\n* [ ] bullet3');
+  });
+
+  it('should remove task syntax on ordered task list', () => {
+    mde.setMarkdown('1. [ ] ordered1\n2. [ ] ordered2\n3. [ ] ordered3');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('1. ordered1\n2. ordered2\n3. ordered3');
+  });
+
+  it('should remove task syntax on bullet task list', () => {
+    mde.setMarkdown('* [ ] bullet1\n* [ ] bullet2\n* [ ] bullet3');
+
+    cmd.exec('markdown', 'selectAll');
+    cmd.exec('markdown', 'task');
+
+    expect(getTextContent(mde)).toEqual('* bullet1\n* bullet2\n* bullet3');
   });
 });
