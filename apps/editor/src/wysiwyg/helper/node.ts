872c25672daf2bae83b1305e77bf5ff38ffb2593
seonim-ryu <seonim.ryu@nhn.com>
2020-10-14 22:32:49 +0900

    refactor: remove unused helper in wysiwyg

--- apps/editor/src/wysiwyg/helper/node.ts
+++
@@ -1,6 +0,0 @@
-export function resolvePosition(docSize: number, from: number, to: number) {
-  const resolvedFrom = Math.min(Math.max(from, 0), docSize);
-  const resolvedTo = Math.min(Math.max(to, 0), docSize);
-
-  return { from: resolvedFrom, to: resolvedTo };
-}
