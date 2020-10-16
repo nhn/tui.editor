3982ba62a0c520233779da49eb8e7bb0ce8d964e
seonim-ryu <seonim.ryu@nhn.com>
2020-10-16 15:06:33 +0900

    feat: add jest setup file for global mocking

---
+++ apps/editor/src/__test__/jest-setup.ts
@@ -0,0 +1,4 @@
+global.Range.prototype.getClientRects = jest.fn().mockReturnValue({
+  length: 1,
+  0: { bottom: 0, height: 0, left: 0, right: 0, top: 0, width: 0, x: 0, y: 0 }
+});
--- apps/editor/src/__test__/wysiwyg/wwCommand.spec.ts
+++ apps/editor/src/__test__/wysiwyg/wwCommand.spec.ts
@@ -6,11 +6,6 @@ import WysiwygEditor from '@/wysiwyg/wwEditor';
 import EventEmitter from '@/event/eventEmitter';
 import CommandManager from '@/commands/commandManager';
 
-global.Range.prototype.getClientRects = jest.fn().mockReturnValue({
-  length: 1,
-  0: { bottom: 0, height: 0, left: 0, right: 0, top: 0, width: 0, x: 0, y: 0 }
-});
-
 describe('wysiwyg commands', () => {
   let container: HTMLElement, wwe: WysiwygEditor, em: EventEmitter, cmd: CommandManager;
 
