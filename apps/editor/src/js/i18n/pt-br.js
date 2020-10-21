8a69afd1e5d3baa2cca580b2e0f1799034c06281
Nícolas Huber <nicolasluishuber@gmail.com>
2020-10-20 22:05:58 -0300

    feat: i18n pt-BR (#1220)
    * Add pt-BR i18n support
    
    * fix: pt-br i18n parameters

---
+++ apps/editor/src/js/i18n/pt-br.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview I18N for Português
+ * @author Nícolas Huber <nicolasluishuber@gmail.com>
+ */
+import Editor from '../editor';
+
+Editor.setLanguage(['pt', 'pt-BR'], {
+  Markdown: 'Markdown',
+  WYSIWYG: 'WYSIWYG',
+  Write: 'Escrever',
+  Preview: 'Pré-visualizar',
+  Headings: 'Cabeçalhos',
+  Paragraph: 'Parágrafo',
+  Bold: 'Negrito',
+  Italic: 'Itálico',
+  Strike: 'Traçado',
+  Code: 'Código',
+  Line: 'Linha',
+  Blockquote: 'Bloco de citação',
+  'Unordered list': 'Lista não ordenada',
+  'Ordered list': 'Lista ordenada',
+  Task: 'Tarefa',
+  Indent: 'Recuo à esquerda',
+  Outdent: 'Recuo à direita',
+  'Insert link': 'Inserir link',
+  'Insert CodeBlock': 'Inserir bloco de código',
+  'Insert table': 'Inserir tabela',
+  'Insert image': 'Inserir imagem',
+  Heading: 'Título',
+  'Image URL': 'URL da imagem',
+  'Select image file': 'Selecione um arquivo de imagem',
+  Description: 'Descrição',
+  OK: 'OK',
+  More: 'Mais',
+  Cancel: 'Cancelar',
+  File: 'Arquivo',
+  URL: 'URL',
+  'Link text': 'Link de texto',
+  'Add row': 'Adicionar linha',
+  'Add col': 'Adicionar coluna',
+  'Remove row': 'Remover linha',
+  'Remove col': 'Remover coluna',
+  'Align left': 'Alinhar à esquerda',
+  'Align center': 'Alinhar ao centro',
+  'Align right': 'Alinhar à direita',
+  'Remove table': 'Remover tabela',
+  'Would you like to paste as table?': 'Você gostaria de colar como mesa?',
+  'Text color': 'Cor do texto',
+  'Auto scroll enabled': 'Rolagem automática habilitada',
+  'Auto scroll disabled': 'Rolagem automática desabilitada',
+  'Choose language': 'Escolher linguagem'
+});
