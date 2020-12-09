15ed5d5716feb71dd7fa3e685c9305ced7fbb9be
류선임 <seonim.ryu@nhn.com>
2020-12-09 21:26:10 +0900

    feat: add context menu for wysiwyg table (#1288)
    * feat: add context menu for wysiwyg table
    
    * fix: wrong text
    
    * feat: apply code review

--- apps/editor/src/ui/popupTableUtils.js
+++ apps/editor/src/ui/popupTableUtils.js
@@ -5,6 +5,7 @@
 import extend from 'tui-code-snippet/object/extend';
 import css from 'tui-code-snippet/domUtil/css';
 import hasClass from 'tui-code-snippet/domUtil/hasClass';
+import isString from 'tui-code-snippet/type/isString';
 
 import LayerPopup from './layerpopup';
 import i18n from '../i18n/i18n';
@@ -13,6 +14,18 @@ import domUtils from '../utils/dom';
 export const REMOVE_ROW_MENU_CLASS_NAME = 'te-table-remove-row';
 export const DISABLED_MENU_CLASS_NAME = 'te-context-menu-disabled';
 
+const tableCommandMap = {
+  'add-row-up': 'addRowToUp',
+  'add-row-down': 'addRowToDown',
+  'add-col-left': 'addColumnToLeft',
+  'add-col-right': 'addColumnToRight',
+  'remove-col': 'removeColumn',
+  'col-align-left': ['alignColumn', { align: 'left' }],
+  'col-align-center': ['alignColumn', { align: 'center' }],
+  'col-align-right': ['alignColumn', { align: 'right' }],
+  'remove-table': 'removeTable'
+};
+
 /**
  * PopupTableUtils
  * It implements table utils popup
@@ -20,17 +33,41 @@ export const DISABLED_MENU_CLASS_NAME = 'te-context-menu-disabled';
  */
 class PopupTableUtils extends LayerPopup {
   constructor(options) {
+    // @TODO change class name of template
     const POPUP_CONTENT = `
-      <button type="button" class="te-table-add-row">${i18n.get('Add row')}</button>
-      <button type="button" class="te-table-add-col">${i18n.get('Add col')}</button>
-      <button type="button" class="te-table-remove-row">${i18n.get('Remove row')}</button>
-      <button type="button" class="te-table-remove-col">${i18n.get('Remove col')}</button>
+      <button type="button" data-table-command-type="add-row-up">${i18n.get(
+        'Add row to up'
+      )}</button>
+      <button type="button" data-table-command-type="add-row-down">${i18n.get(
+        'Add row to down'
+      )}</button>
+      <button type="button" class="te-table-remove-row" data-table-command-type="remove-row">${i18n.get(
+        'Remove row'
+      )}</button>
       <hr/>
-      <button type="button" class="te-table-col-align-left">${i18n.get('Align left')}</button>
-      <button type="button" class="te-table-col-align-center">${i18n.get('Align center')}</button>
-      <button type="button" class="te-table-col-align-right">${i18n.get('Align right')}</button>
+      <button type="button" data-table-command-type="add-col-left">${i18n.get(
+        'Add column to left'
+      )}</button>
+      <button type="button" data-table-command-type="add-col-right">${i18n.get(
+        'Add column to right'
+      )}</button>
+      <button type="button" data-table-command-type="remove-col">${i18n.get(
+        'Remove column'
+      )}</button>
       <hr/>
-      <button type="button" class="te-table-remove">${i18n.get('Remove table')}</button>
+      <button type="button" data-table-command-type="col-align-left">${i18n.get(
+        'Align column to left'
+      )}</button>
+      <button type="button" data-table-command-type="col-align-center">${i18n.get(
+        'Align column to center'
+      )}</button>
+      <button type="button" data-table-command-type="col-align-right">${i18n.get(
+        'Align column to right'
+      )}</button>
+      <hr/>
+      <button type="button" data-table-command-type="remove-table">${i18n.get(
+        'Remove table'
+      )}</button>
     `;
 
     options = extend(
@@ -64,19 +101,32 @@ class PopupTableUtils extends LayerPopup {
   _initDOMEvent() {
     super._initDOMEvent();
 
-    this.on('click .te-table-add-row', () => this.eventEmitter.emit('command', 'AddRow'));
-    this.on('click .te-table-add-col', () => this.eventEmitter.emit('command', 'AddCol'));
-    this.on('click .te-table-col-align-left', () =>
-      this.eventEmitter.emit('command', 'AlignCol', 'left')
-    );
-    this.on('click .te-table-col-align-center', () =>
-      this.eventEmitter.emit('command', 'AlignCol', 'center')
-    );
-    this.on('click .te-table-col-align-right', () =>
-      this.eventEmitter.emit('command', 'AlignCol', 'right')
-    );
-    this.on('click .te-table-remove-col', () => this.eventEmitter.emit('command', 'RemoveCol'));
-    this.on('click .te-table-remove', () => this.eventEmitter.emit('command', 'RemoveTable'));
+    this.on('click', ({ target }) => {
+      const commandType = target.dataset.tableCommandType;
+
+      console.log(target.dataset);
+
+      if (commandType) {
+        const commandInfo = tableCommandMap[commandType];
+
+        if (!commandInfo) {
+          return;
+        }
+
+        let command = commandInfo;
+        let payload = null;
+
+        if (!isString(commandInfo)) {
+          command = commandInfo[0];
+          payload = commandInfo[1];
+        }
+
+        this.eventEmitter.emit('command', { type: 'wysiwyg', command }, payload);
+      }
+
+      this.hide();
+    });
+
     this._bindClickEventOnRemoveRowMenu();
   }
 
@@ -113,7 +163,7 @@ class PopupTableUtils extends LayerPopup {
       if (hasClass(target, DISABLED_MENU_CLASS_NAME)) {
         ev.preventDefault();
       } else {
-        this.eventEmitter.emit('command', 'RemoveRow');
+        this.eventEmitter.emit('command', { type: 'wysiwyg', command: 'removeRow' });
       }
     });
   }
