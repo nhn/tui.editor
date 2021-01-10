db1af402775966ce40f11cc96bfc46c3caea8f68
js87zz <js87zz.lee@nhn.com>
2021-01-11 00:37:38 +0900

    feat: add tab component

---
+++ apps/editor/src/new/components/tabs.ts
@@ -0,0 +1,34 @@
+import { TabInfo } from '@t/ui';
+import i18n from '@/i18n/i18n';
+import html from '../vdom/template';
+import { Component } from '../vdom/component';
+
+interface Props {
+  tabs: TabInfo[];
+  activeTab: string;
+  onClick: (ev: MouseEvent, activeTab: string) => void;
+}
+
+export class Tabs extends Component<Props> {
+  private toggleTab(ev: MouseEvent, activeTab: string) {
+    this.props.onClick(ev, activeTab);
+  }
+
+  render() {
+    return html`
+      <div class="te-tab">
+        ${this.props.tabs.map(
+          ({ name, text }) => html`
+            <button
+              type="button"
+              class="${this.props.activeTab === name ? 'te-tab-active' : ''}"
+              onClick=${(ev: MouseEvent) => this.toggleTab(ev, name)}
+            >
+              ${i18n.get(text)}
+            </button>
+          `
+        )}
+      </div>
+    `;
+  }
+}
