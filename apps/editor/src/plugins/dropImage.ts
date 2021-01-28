4c59bd140507e0fdc60455df6f8061401c19bf98
js87zz <js87zz.lee@nhn.com>
2021-01-28 12:49:21 +0900

    feat: add common image hook and dropImage plugin

---
+++ apps/editor/src/plugins/dropImage.ts
@@ -0,0 +1,34 @@
+import { Plugin } from 'prosemirror-state';
+import forEachArray from 'tui-code-snippet/collection/forEachArray';
+import { Context } from '@t/spec';
+import { emitImageBlobHook } from '@/helper/image';
+import { EditorType } from '@t/editor';
+
+export function dropImage({ eventEmitter }: Context, editorType: EditorType) {
+  return new Plugin({
+    props: {
+      handleDOMEvents: {
+        drop: (_, ev) => {
+          const items = (ev as DragEvent).dataTransfer?.files;
+
+          if (items) {
+            forEachArray(items, (item) => {
+              if (item.type.indexOf('image') !== -1) {
+                ev.preventDefault();
+                ev.stopPropagation();
+
+                const blob = item.name ? item : item.getAsFile(); // Blob or File
+
+                emitImageBlobHook(eventEmitter, editorType, blob, ev.type);
+
+                return false;
+              }
+              return true;
+            });
+          }
+          return true;
+        },
+      },
+    },
+  });
+}
