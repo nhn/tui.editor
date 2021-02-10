8e31e3a076805078636eac18be988ae00466ab1f
yar2001 <yar2001@163.com>
2021-02-10 17:44:58 +0800

    fix: React-editor 'onEvent' props missing (#1292)
    * Fix React-editor 'onEvent' props missing
    
    Fix React-editor 'onEvent' props missing, or it will cause a fatal error when using TypeScript.
    
    * Update index.d.ts
    
    * Update index.d.ts

--- apps/react-editor/index.d.ts
+++ apps/react-editor/index.d.ts
@@ -1,8 +1,15 @@
 import { Component } from 'react';
-import ToastuiEditor, { EditorOptions } from '@toast-ui/editor';
+import ToastuiEditor, { EditorOptions, SourceType, MarkdownToolbarState, WysiwygToolbarState } from '@toast-ui/editor';
 import ToastuiEditorViewer, { ViewerOptions } from '@toast-ui/editor/dist/toastui-editor-viewer';
 
-type EditorProps = Omit<EditorOptions, 'el'>;
+
+type EditorProps = Omit<EditorOptions & {
+  onLoad?: (param: Editor) => void;
+  onChange?: (param: { source: SourceType | 'viewer'; data: MouseEvent }) => void;
+  onStateChange?: (param: MarkdownToolbarState | WysiwygToolbarState) => void;
+  onFocus?: (param: { source: SourceType }) => void;
+  onBlur?: (param: { source: SourceType }) => void;
+}, 'el'>;
 type ViewerProps = Omit<ViewerOptions, 'el'>;
 
 export class Editor extends Component<EditorProps> {
