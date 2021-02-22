5da1575ecb4f0fda48ae4f5c998194b1f38bf8e6
js87zz <js87zz.lee@nhn.com>
2021-02-22 09:40:19 +0900

    wip: add test case(editor API)

---
+++ apps/editor/src/__test__/unit/editor.spec.ts
@@ -0,0 +1,272 @@
+import { oneLineTrim } from 'common-tags';
+import Editor from '@/editorCore';
+import Viewer from '@/Viewer';
+import i18n from '@/i18n/i18n';
+
+describe('editor', () => {
+  let container: HTMLElement,
+    mdEditor: HTMLElement,
+    mdPreview: HTMLElement,
+    wwEditor: HTMLElement,
+    editor: Editor;
+
+  function getPreviewHTML() {
+    return mdPreview
+      .querySelector('.tui-editor-contents')!
+      .innerHTML.replace(/\sdata-nodeid="\d+"/g, '')
+      .trim();
+  }
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    editor = new Editor({
+      el: container,
+      previewHighlight: false,
+      widgetRules: [
+        {
+          rule: /@\S+/,
+          toDOM(text) {
+            const span = document.createElement('span');
+
+            span.innerHTML = `<a href="www.google.com">${text}</a>`;
+            return span;
+          },
+        },
+      ],
+    });
+
+    const elements = editor.getEditorElements();
+
+    mdEditor = elements.mdEditor;
+    mdPreview = elements.mdPreview!;
+    wwEditor = elements.wwEditor!;
+
+    container.append(mdEditor);
+    container.append(mdPreview!);
+    container.append(wwEditor!);
+
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    editor.destroy();
+    document.body.removeChild(container);
+  });
+
+  describe('instance API', () => {
+    it('setPlaceholder()', () => {
+      editor.setPlaceholder('Please input text');
+
+      const expected = '<span class="placeholder ProseMirror-widget">Please input text</span>';
+
+      expect(mdEditor).toContainHTML(expected);
+      expect(wwEditor).toContainHTML(expected);
+    });
+
+    it('changeMode()', () => {
+      const spy = jest.fn();
+
+      expect(editor.isMarkdownMode()).toBe(true);
+      expect(editor.isWysiwygMode()).toBe(false);
+
+      editor.on('changeMode', spy);
+      editor.changeMode('wysiwyg');
+
+      expect(spy).toHaveBeenCalledWith('wysiwyg');
+      expect(editor.isMarkdownMode()).toBe(false);
+      expect(editor.isWysiwygMode()).toBe(true);
+    });
+
+    it('changePreviewStyle()', () => {
+      const spy = jest.fn();
+
+      expect(editor.getCurrentPreviewStyle()).toBe('tab');
+
+      editor.on('changePreviewStyle', spy);
+      editor.changePreviewStyle('vertical');
+
+      expect(spy).toHaveBeenCalledWith('vertical');
+      expect(editor.getCurrentPreviewStyle()).toBe('vertical');
+    });
+
+    it('getRange()', () => {
+      // markdown range
+      expect(editor.getRange()).toEqual([
+        [1, 1],
+        [1, 1],
+      ]);
+
+      editor.changeMode('wysiwyg');
+
+      // wysiwyg range
+      expect(editor.getRange()).toEqual([1, 1]);
+    });
+
+    it('setMarkdown()', () => {
+      editor.setMarkdown('# heading');
+
+      expect(mdEditor).toContainHTML(
+        '<div><span class="tui-md-heading tui-md-heading1"><span class="tui-md-delimiter">#</span> heading</span></div>'
+      );
+      expect(getPreviewHTML()).toBe('<h1>heading</h1>');
+    });
+
+    it('setHTML()', () => {
+      editor.setHTML('<h1>heading</h1>');
+
+      expect(mdEditor).toContainHTML(
+        '<div><span class="tui-md-heading tui-md-heading1"><span class="tui-md-delimiter">#</span> heading</span></div>'
+      );
+      expect(getPreviewHTML()).toBe('<h1>heading</h1>');
+    });
+
+    it('reset()', () => {
+      editor.setMarkdown('# heading');
+      editor.reset();
+
+      expect(mdEditor).not.toContainHTML(
+        '<div><span class="tui-md-heading tui-md-heading1"><span class="tui-md-delimiter">#</span> heading</span></div>'
+      );
+      expect(getPreviewHTML()).toBe('');
+    });
+
+    it('getCurrentModeEditor()', () => {
+      // @ts-ignore
+      expect(editor.getCurrentModeEditor()).toEqual(editor.mdEditor);
+
+      editor.changeMode('wysiwyg');
+
+      // @ts-ignore
+      expect(editor.getCurrentModeEditor()).toEqual(editor.wwEditor);
+    });
+
+    it('setMinHeight()', () => {
+      editor.setMinHeight('200px');
+
+      expect(mdEditor).toHaveStyle({ minHeight: '200px' });
+      expect(mdPreview).toHaveStyle({ minHeight: '200px' });
+      expect(wwEditor).toHaveStyle({ minHeight: '200px' });
+    });
+
+    describe('setHeight()', () => {
+      it('should set height with pixel option', () => {
+        editor.setHeight('300px');
+
+        expect(container).not.toHaveClass('auto-height');
+        expect(container).toHaveStyle({ height: '300px' });
+        expect(mdEditor).toHaveStyle({ minHeight: '300px' });
+        expect(mdPreview).toHaveStyle({ minHeight: '300px' });
+        expect(wwEditor).toHaveStyle({ minHeight: '300px' });
+      });
+
+      it('should set height with auto option', () => {
+        editor.setHeight('auto');
+
+        expect(container).toHaveClass('auto-height');
+        expect(container).toHaveStyle({ height: 'auto' });
+        expect(mdEditor).toHaveStyle({ minHeight: '300px' });
+        expect(mdPreview).toHaveStyle({ minHeight: '300px' });
+        expect(wwEditor).toHaveStyle({ minHeight: '300px' });
+      });
+    });
+
+    // it('insertText()', () => {
+    //   editor.insertText('test');
+
+    //   expect(mdEditor).not.toContainHTML('<div>test</div>');
+    //   expect(getPreviewHTML()).toBe('test');
+    // });
+
+    it('addWidget()', () => {
+      const node = document.createElement('div');
+
+      node.innerHTML = 'widget';
+
+      editor.addWidget(node, 'top');
+
+      expect(document.body).toContainElement(node);
+
+      editor.changeMode('wysiwyg');
+
+      expect(document.body).not.toContainElement(node);
+    });
+
+    describe('replaceWithWidget()', () => {
+      it('in markdown', () => {
+        editor.replaceWithWidget([1, 1], [1, 1], '@test');
+
+        const expectedEditor = oneLineTrim`
+          <span class="tui-widget">
+            <span><a href="www.google.com">@test</a></span>
+          </span>
+        `;
+        const expectedPreview = oneLineTrim`
+          <p>
+            <span class="tui-widget">
+              <span><a href="www.google.com">@test</a></span>
+            </span>
+          </p>
+        `;
+
+        expect(mdEditor).toContainHTML(expectedEditor);
+        expect(getPreviewHTML()).toBe(expectedPreview);
+      });
+
+      it('in wysiwyg', () => {
+        editor.changeMode('wysiwyg');
+        editor.replaceWithWidget(1, 1, '@test');
+
+        const expected = oneLineTrim`
+          <span class="tui-widget">
+            <span><a href="www.google.com">@test</a></span>
+          </span>
+        `;
+
+        expect(wwEditor).toContainHTML(expected);
+      });
+    });
+
+    it('exec()', () => {
+      // @ts-ignore
+      jest.spyOn(editor.commandManager, 'exec');
+
+      editor.exec('markdown', 'bold');
+
+      // @ts-ignore
+      // eslint-disable-next-line no-undefined
+      expect(editor.commandManager.exec).toHaveBeenCalledWith('markdown', 'bold', undefined);
+    });
+
+    it('addCommand()', () => {
+      const handler = jest.fn();
+
+      // @ts-ignore
+      jest.spyOn(editor.commandManager, 'addCommand');
+
+      editor.addCommand('markdown', 'custom', handler);
+
+      // @ts-ignore
+      expect(editor.commandManager.addCommand).toHaveBeenCalledWith('markdown', 'custom', handler);
+    });
+  });
+
+  describe('static API', () => {
+    it('factory()', () => {
+      const editorInst = Editor.factory({ el: document.createElement('div'), viewer: false });
+      const viewerInst = Editor.factory({ el: document.createElement('div'), viewer: true });
+
+      expect(editorInst).toBeInstanceOf(Editor);
+      expect(viewerInst).toBeInstanceOf(Viewer);
+    });
+
+    it('setLanguage()', () => {
+      const data = {};
+
+      jest.spyOn(i18n, 'setLanguage');
+
+      Editor.setLanguage('ko', data);
+
+      expect(i18n.setLanguage).toHaveBeenCalledWith('ko', data);
+    });
+  });
+});
