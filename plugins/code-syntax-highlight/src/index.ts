1ef40b1eb473ba5ca52701f8065db30d63c587fc
seonim-ryu <seonim.ryu@nhn.com>
2021-03-09 11:08:16 +0900

    feat: add highlight and lowlight

--- plugins/code-syntax-highlight/src/index.ts
+++ plugins/code-syntax-highlight/src/index.ts
@@ -2,4 +2,7 @@ import { codeSyntaxHighlightPlugin } from '@/plugin';
 
 import '@/css/plugin.css';
 
-export default codeSyntaxHighlightPlugin;
+import low from 'lowlight/lib/core';
+
+export default (eventEmitter: any, options = {}) =>
+  codeSyntaxHighlightPlugin(eventEmitter, low, options);
--- plugins/code-syntax-highlight/src/plugin.ts
+++ plugins/code-syntax-highlight/src/plugin.ts
@@ -1,17 +1,27 @@
-import { toHTMLRenderers } from '@/renderers/toHTMLRenderers';
+import { getHTMLRenderers } from '@/renderers/toHTMLRenderers';
 import { codeSyntaxHighlighting } from '@/plugins/codeSyntaxHighlighting';
 
 import { PluginOptions } from '@t/plugin';
 
-export function codeSyntaxHighlightPlugin(eventEmitter: any, options = {}) {
-  const { hljs } = options as PluginOptions;
-  const plugins = [
-    {
-      editorType: 'wysiwyg',
-      plugin: (evtEmitter: any, toDOMAdaptor: any) =>
-        codeSyntaxHighlighting(evtEmitter, toDOMAdaptor, hljs),
-    },
-  ];
+function registerLanguages(hljs: any, low: any) {
+  const languages: any[] = hljs.listLanguages();
 
-  return { toHTMLRenderers, plugins };
+  languages.forEach((lang) => {
+    const { rawDefinition } = hljs.getLanguage(lang);
+
+    low.registerLanguage(lang, rawDefinition);
+  });
+}
+
+export function codeSyntaxHighlightPlugin(eventEmitter: any, low: any, options = {}) {
+  const { hljs } = options as PluginOptions;
+
+  registerLanguages(hljs, low);
+
+  console.log(low.highlight('js', 'var foo = 123'));
+
+  return {
+    toHTMLRenderers: getHTMLRenderers(hljs),
+    wwPlugins: [() => codeSyntaxHighlighting(low)],
+  };
 }
