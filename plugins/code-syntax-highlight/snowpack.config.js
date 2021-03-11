035346f2b5d05bc4aa4b2a57ce7f66da8d028413
seonim-ryu <seonim.ryu@nhn.com>
2021-03-11 11:56:04 +0900

    fix: edit demo

--- plugins/code-syntax-highlight/snowpack.config.js
+++ plugins/code-syntax-highlight/snowpack.config.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createServer({ target: 'http://localhost:8080' });
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    esm: '/',
+    'demo-esm': '/',
     src: '/dist',
   },
   packageOptions: {
@@ -10,6 +14,14 @@ module.exports = {
   devOptions: {
     port: 8081,
   },
+  routes: [
+    {
+      src: '/img/.*',
+      dest: (req, res) => {
+        proxy.web(req, res);
+      },
+    },
+  ],
   buildOptions: {
     clean: true,
     sourceMaps: false,
