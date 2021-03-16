b2a1b217bc328899d7e261ee239a5eacdf4bc5a0
js87zz <js87zz.lee@nhn.com>
2021-03-16 10:27:00 +0900

    chore: fix jest setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 
-global.Range.prototype.getClientRects = jest.fn().mockReturnValue({ length: 0 });
-
-global.Range.prototype.getBoundingClientRect = jest.fn().mockReturnValue({});
+if (global.Range) {
+  global.Range.prototype.getClientRects = jest.fn().mockReturnValue({ length: 0 });
+  global.Range.prototype.getBoundingClientRect = jest.fn().mockReturnValue({});
+}
