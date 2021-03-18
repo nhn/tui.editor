81d6dab61c4fa93d3a10b54306f10102d84155b2
js87zz <js87zz.lee@nhn.com>
2021-03-18 17:16:38 +0900

    feat: add uml plugin

--- plugins/uml/src/index.ts
+++ plugins/uml/src/index.ts
@@ -3,43 +3,46 @@
  * @author NHN FE Development Lab <dl_javascript@nhn.com>
  */
 import plantumlEncoder from 'plantuml-encoder';
+import { UMLPluginOptions } from '../index';
 
 const DEFAULT_RENDERER_URL = 'http://www.plantuml.com/plantuml/png/';
-const UML_LANGUAGES = ['uml', 'plantuml'];
+
+function createUMLTokens(text: string, rendererURL: string) {
+  let renderedHTML;
+
+  try {
+    if (!plantumlEncoder) {
+      throw new Error('plantuml-encoder dependency required');
+    }
+    renderedHTML = `<img src="${rendererURL}${plantumlEncoder.encode(text)}" />`;
+  } catch (err) {
+    renderedHTML = `Error occurred on encoding uml: ${err.message}`;
+  }
+
+  return [
+    { type: 'openTag', tagName: 'div', outerNewLine: true },
+    { type: 'html', content: renderedHTML },
+    { type: 'closeTag', tagName: 'div', outerNewLine: true },
+  ];
+}
 
 /**
  * UML plugin
- * @param {Editor|Viewer} editor - instance of Editor or Viewer
+ * @param {Object} emitter - event emitter for communicating with editor
  * @param {Object} options - options for plugin
  * @param {string} [options.rendererURL] - url of plant uml renderer
  */
-export default function umlPlugin(editor, options = {}) {
+export default function umlPlugin(_: any, options: UMLPluginOptions = {}) {
   const { rendererURL = DEFAULT_RENDERER_URL } = options;
 
-  /**
-   * render html from uml
-   * @param {string} umlCode - plant uml code text
-   * @returns {string} - rendered html
-   */
-  function plantUMLReplacer(umlCode) {
-    let renderedHTML;
-
-    try {
-      if (!plantumlEncoder) {
-        throw new Error('plantuml-encoder dependency required');
-      }
-      renderedHTML = `<img src="${rendererURL}${plantumlEncoder.encode(umlCode)}" />`;
-    } catch (err) {
-      renderedHTML = `Error occurred on encoding uml: ${err.message}`;
-    }
-
-    return renderedHTML;
-  }
-
-  editor.setCodeBlockLanguages(UML_LANGUAGES);
-  UML_LANGUAGES.forEach((umlLanguage) => {
-    const { codeBlockManager } = Object.getPrototypeOf(editor).constructor;
-
-    codeBlockManager.setReplacer(umlLanguage, plantUMLReplacer);
-  });
+  return {
+    toHTMLRenderers: {
+      uml(node: any) {
+        return createUMLTokens(node.literal!, rendererURL);
+      },
+      plantUml(node: any) {
+        return createUMLTokens(node.literal!, rendererURL);
+      },
+    },
+  };
 }
