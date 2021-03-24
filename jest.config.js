c6bbf6ddf9a16253235e5ee9d9c7470d30f1fda3
js87zz <js87zz.lee@nhn.com>
2021-03-24 18:33:23 +0900

    chore: add color plugin test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   projects: [
     '<rootDir>/libs/toastmark/jest.config.js',
     '<rootDir>/apps/editor/jest.config.js',
+    '<rootDir>/plugins/color-syntax/jest.config.js',
     '<rootDir>/plugins/chart/jest.config.js',
   ],
 };
