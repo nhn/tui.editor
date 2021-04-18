029ebf242487e416ebda382d4c8b135cb269209e
js87zz <js87zz.lee@nhn.com>
2021-04-19 02:33:04 +0900

    refactor: context menu DOM structure

--- apps/editor/src/wysiwyg/plugins/tableContextMenu.ts
+++ apps/editor/src/wysiwyg/plugins/tableContextMenu.ts
@@ -12,38 +12,65 @@ interface ContextMenuInfo {
   payload?: {
     align: string;
   };
+  className: string;
   disableInThead?: boolean;
 }
 
 const contextMenuGroups: ContextMenuInfo[][] = [
   [
-    { action: 'Add row to up', command: 'addRowToUp', disableInThead: true },
-    { action: 'Add row to down', command: 'addRowToDown', disableInThead: true },
-    { action: 'Remove row', command: 'removeRow', disableInThead: true },
+    {
+      action: 'Add row to up',
+      command: 'addRowToUp',
+      disableInThead: true,
+      className: 'add-row-up',
+    },
+    {
+      action: 'Add row to down',
+      command: 'addRowToDown',
+      disableInThead: true,
+      className: 'add-row-down',
+    },
+    { action: 'Remove row', command: 'removeRow', disableInThead: true, className: 'remove-row' },
   ],
   [
-    { action: 'Add column to left', command: 'addColumnToLeft' },
-    { action: 'Add column to right', command: 'addColumnToRight' },
-    { action: 'Remove column', command: 'removeColumn' },
+    { action: 'Add column to left', command: 'addColumnToLeft', className: 'add-column-left' },
+    { action: 'Add column to right', command: 'addColumnToRight', className: 'add-column-right' },
+    { action: 'Remove column', command: 'removeColumn', className: 'remove-column' },
   ],
   [
-    { action: 'Align column to left', command: 'alignColumn', payload: { align: 'left' } },
-    { action: 'Align column to center', command: 'alignColumn', payload: { align: 'center' } },
-    { action: 'Align column to right', command: 'alignColumn', payload: { align: 'right' } },
+    {
+      action: 'Align column to left',
+      command: 'alignColumn',
+      payload: { align: 'left' },
+      className: 'align-column-left',
+    },
+    {
+      action: 'Align column to center',
+      command: 'alignColumn',
+      payload: { align: 'center' },
+      className: 'align-column-center',
+    },
+    {
+      action: 'Align column to right',
+      command: 'alignColumn',
+      payload: { align: 'right' },
+      className: 'align-column-right',
+    },
   ],
-  [{ action: 'Remove table', command: 'removeTable' }],
+  [{ action: 'Remove table', command: 'removeTable', className: 'remove-table' }],
 ];
 
 function getContextMenuGroups(eventEmitter: Emitter, inTableHead: boolean) {
   return contextMenuGroups
     .map((contextMenuGroup) =>
-      contextMenuGroup.map(({ action, command, payload, disableInThead }) => {
+      contextMenuGroup.map(({ action, command, payload, disableInThead, className }) => {
         return {
           label: i18n.get(action),
           onClick: () => {
             eventEmitter.emit('command', command, payload);
           },
           disabled: inTableHead && !!disableInThead,
+          className,
         };
       })
     )
