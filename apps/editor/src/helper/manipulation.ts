396ab38d43b4245424e2f83d834ceeba8f519fed
js87zz <js87zz.lee@nhn.com>
2021-05-18 01:04:00 +0900

    refactor: remove unnecessary function

--- apps/editor/src/helper/manipulation.ts
+++ apps/editor/src/helper/manipulation.ts
@@ -3,37 +3,6 @@ import { ProsemirrorNode, Schema, Mark, ResolvedPos } from 'prosemirror-model';
 
 import isString from 'tui-code-snippet/type/isString';
 
-export function replaceNodes(
-  tr: Transaction,
-  from: number,
-  to: number,
-  nodes: ProsemirrorNode | ProsemirrorNode[],
-  diff: { from: number; to: number } = { from: 1, to: 1 }
-) {
-  const resolvedFrom = Math.max(from - diff.from, 0);
-  const resolvedTo = Math.min(to + diff.to, tr.doc.content.size);
-
-  return (
-    tr
-      .replaceWith(resolvedFrom, resolvedTo, nodes)
-      // To prevent incorrect calculation of the position for markdown parser
-      .setMeta('resolvedPos', [from, to])
-  );
-}
-
-export function insertNodes(
-  tr: Transaction,
-  pos: number,
-  nodes: ProsemirrorNode | ProsemirrorNode[]
-) {
-  return (
-    tr
-      .insert(pos, nodes)
-      // To prevent incorrect calculation of the position for markdown parser
-      .setMeta('resolvedPos', [pos, pos])
-  );
-}
-
 export function createParagraph(schema: Schema, content?: string | ProsemirrorNode[]) {
   const { paragraph } = schema.nodes;
 
