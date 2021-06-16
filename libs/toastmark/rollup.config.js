1ed1cc1e5172b2833385bb3b613f8b44a24ee209
js87zz <js87zz.lee@nhn.com>
2021-06-16 22:17:46 +0900

    chore: build toastmark with rollup

---
+++ libs/toastmark/rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist/esm',
+      format: 'es',
+      sourcemap: false,
+    },
+    plugins: [typescript(), commonjs(), nodeResolve(), json()],
+  },
+];
