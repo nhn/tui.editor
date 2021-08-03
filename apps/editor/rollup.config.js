b0139a09361e9b59e3be03d743111ea78a33d46b
jaesung-lee <js87zz.lee@nhn.com>
2021-08-03 11:41:23 +0900

    fix: cannot import i18n bundle (#1720)
    * fix: add i18n to exports field
    
    * fix: add exports field to esm i18n build

--- apps/editor/rollup.config.js
+++ apps/editor/rollup.config.js
@@ -1,8 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import fs from 'fs';
+
+function i18nEditorImportPath() {
+  return {
+    name: 'i18nEditorImportPath',
+    transform(code) {
+      return code.replace('../editorCore', '@toast-ui/editor');
+    },
+  };
+}
+
+const fileNames = fs.readdirSync('./src/i18n');
 
 export default [
+  // editor
   {
     input: 'src/esm/index.ts',
     output: {
@@ -13,6 +26,7 @@ export default [
     plugins: [typescript(), commonjs(), nodeResolve()],
     external: [/^prosemirror/],
   },
+  // viewer
   {
     input: 'src/esm/indexViewer.ts',
     output: {
@@ -22,5 +36,16 @@ export default [
     },
     plugins: [typescript(), commonjs(), nodeResolve()],
     external: [/^prosemirror/],
+  },
+  // i18n
+  {
+    input: fileNames.map((fileName) => `src/i18n/${fileName}`),
+    output: {
+      dir: 'dist/esm/i18n',
+      format: 'es',
+      sourcemap: false,
+    },
+    external: ['@toast-ui/editor'],
+    plugins: [typescript(), commonjs(), nodeResolve(), i18nEditorImportPath()],
   },
 ];
