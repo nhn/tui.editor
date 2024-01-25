3dc4ab703307c797eeb4efb413864a472ceb40f3
Hexway Team <contact@hexway.io>
2024-01-25 13:16:14 +0400

    Bug: escape XML-entities in codeSyntaxHighlight plugin when language is unspecified (fix #3220)

--- plugins/code-syntax-highlight/src/utils/common.ts
+++ plugins/code-syntax-highlight/src/utils/common.ts
@@ -1,3 +1,28 @@
 export function flatten<T>(arr: T[]): T[] {
   return arr.reduce<T[]>((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 }
+
+const XMLSPECIAL = '[&<>"]';
+const reXmlSpecial = new RegExp(XMLSPECIAL, 'g');
+
+function replaceUnsafeChar(char: string) {
+  switch (char) {
+    case '&':
+      return '&amp;';
+    case '<':
+      return '&lt;';
+    case '>':
+      return '&gt;';
+    case '"':
+      return '&quot;';
+    default:
+      return char;
+  }
+}
+
+export function escapeXml(text: string) {
+  if (reXmlSpecial.test(text)) {
+    return text.replace(reXmlSpecial, replaceUnsafeChar);
+  }
+  return text;
+}
